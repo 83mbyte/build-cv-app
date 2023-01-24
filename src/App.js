abc6eec2a5d0272259289dff8d24667e54a5aa2d
83mbyte <registered.mail.mb@gmail.com>
2023-01-24 14:10:37 +0200

    load state from online to globalState variable

--- src/App.js
+++ src/App.js
@@ -1,31 +1,37 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import {
-
   Box,
-
   Progress,
   Center,
 } from '@chakra-ui/react';
+
 import './App.css';
 
+import { fetchAPI } from './API/api';
 
 function App() {
   const [globalState, setGlobalState] = useState(null);
+
+  const getDataToState = async () => {
+    let data = await fetchAPI.simpleFetchData('user_1');
+    if (data && data !== 'Error -- simpleFetchData from api.js') {
+      setGlobalState(data);
+    }
+  }
+  useEffect(() => { getDataToState() }, []);
 
   return (
 
     <>
       {
-        globalState ? <p>loaded</p> :
+        globalState
+          ? <p>Loaded State</p>
+          :
           <Center h={'300px'} >
             <Box bg='blue' w={['200px', 'md', 'lg', '2xl']}><Progress isIndeterminate size='xs' /></Box>
           </Center>
-
       }
     </>
-
-
-
 
   );
 }
