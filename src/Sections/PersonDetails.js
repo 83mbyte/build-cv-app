0387c171f999d911042dee6a2163d50c472e6e39
83mbyte <registered.mail.mb@gmail.com>
2023-01-24 15:55:47 +0200

    init commit

---
+++ src/Sections/PersonDetails.js
@@ -0,0 +1,29 @@
+import { Box } from '@chakra-ui/react';
+import React, { Fragment } from 'react';
+import InputCustom from '../__test_temp/InputCustom';
+import SectionContainer from './SectionContainer';
+
+const PersonDetails = ({ title, state, user }) => {
+
+    const rootPath = 'personDetails';
+    return (
+        <SectionContainer type={'grid'} headingTxt={title}>
+            <Fragment>
+                {
+                    Object.keys(state).reverse().map((item, index) => {
+
+                        let basePath = `${item}`;
+                        return Object.keys(state[item]).map((i, ind) => {
+                            let subPath = `${i}`
+                            let path = rootPath + '/' + basePath + '/' + subPath + '/value';
+                            return <InputCustom key={`${ind}_${index}`} labelText={state[item][i].label} defValue={state[item][i].value} path={path} required={state[item][i].required} user={user} />
+                        })
+                    })
+
+                }
+            </Fragment>
+        </SectionContainer>
+    );
+};
+
+export default PersonDetails;
---
+++ src/Sections/SectionContainer.js
@@ -0,0 +1,34 @@
+import { Box, Flex, Heading, SimpleGrid } from '@chakra-ui/react';
+import React from 'react';
+
+const SectionContainer = ({ children, headingTxt, type, flexDirect = 'row' }) => {
+    const switchLayout = () => {
+        switch (type) {
+            case 'grid':
+                return (
+                    <SimpleGrid columns={[1, 1, 2]} py='2'>
+                        {children}
+                    </SimpleGrid>
+                )
+            case 'flex':
+                return (
+                    <Flex py='2' direction={flexDirect}>
+                        {children}
+                    </Flex>
+                )
+            default:
+                break;
+        }
+    }
+
+    return (
+        <Box as='section' px='3' py='3' w='100%' maxW='1024px'  >
+            <Heading as='h3' size={['xs', 'xs', 'md']}>{headingTxt}</Heading>
+            {
+                switchLayout()
+            }
+        </Box>
+    );
+};
+
+export default SectionContainer;
