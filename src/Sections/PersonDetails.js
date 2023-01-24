8af1ceba42c615230c0a0b80ddcb15d99dfb91e7
83mbyte <registered.mail.mb@gmail.com>
2023-01-24 17:20:58 +0200

    added pathway to save data

--- src/Sections/PersonDetails.js
+++ src/Sections/PersonDetails.js
@@ -5,18 +5,25 @@ import SectionContainer from './SectionContainer';
 
 const PersonDetails = ({ title, state, user }) => {
 
-    const rootPath = 'personDetails';
+    //const rootPath = 'personDetails';
     return (
         <SectionContainer type={'grid'} headingTxt={title}>
             <Fragment>
                 {
                     Object.keys(state).reverse().map((item, index) => {
 
-                        let basePath = `${item}`;
+                        // let basePath = `${item}`;
                         return Object.keys(state[item]).map((i, ind) => {
-                            let subPath = `${i}`
-                            let path = rootPath + '/' + basePath + '/' + subPath + '/value';
-                            return <InputCustom key={`${ind}_${index}`} labelText={state[item][i].label} defValue={state[item][i].value} path={path} required={state[item][i].required} user={user} />
+                            // let subPath = `${i}`
+                            // let path = rootPath + '/' + basePath + '/' + subPath + '/value';
+                            return <InputCustom
+                                key={`${ind}_${index}`}
+                                labelText={state[item][i].label}
+                                defValue={state[item][i].value}
+                                path={state[item][i].path}
+                                required={state[item][i].required}
+                                user={user}
+                            />
                         })
                     })
 
