a5d2573f01de68bef59818b3ba8ee9aaaa8a4e11
83mbyte <registered.mail.mb@gmail.com>
2023-01-24 17:18:35 +0200

    WYSIWYG component added

--- src/Sections/ProfSummary.js
+++ src/Sections/ProfSummary.js
@@ -1,12 +1,14 @@
 import { Box, Text } from '@chakra-ui/react';
 import React from 'react';
+import Wysiwyg from '../FormElements/WYSIWYG/Wysiwyg';
 import SectionContainer from './SectionContainer';
 import SectionDescription from './SectionDescription';
 
-const ProfSummary = ({ title }) => {
+const ProfSummary = ({ title, state, user }) => {
     return (
         <SectionContainer headingTxt={title} type="flex" flexDirect='column'>
             <SectionDescription value={"Write 2-4 short & energetic sentences to interest the reader! Mention your role, experience & most importantly - your biggest achievements, best qualities and skills."} />
+            <Wysiwyg state={state} user={user} />
 
 
         </SectionContainer>
