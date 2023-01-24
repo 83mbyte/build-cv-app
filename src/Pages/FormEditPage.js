74c1cadd834625f1e26e7b4ac6301e51dd1f086d
83mbyte <registered.mail.mb@gmail.com>
2023-01-24 14:22:01 +0200

    init commit

---
+++ src/Pages/FormEditPage.js
@@ -0,0 +1,11 @@
+import React, { Fragment } from 'react';
+
+const FormEditPage = ({ state }) => {
+    return (
+        <Fragment>
+            lorem ipsum
+        </Fragment>
+    );
+};
+
+export default FormEditPage;
