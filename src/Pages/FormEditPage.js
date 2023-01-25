0c4b12bbf012cabda3c9703b6a64dba2c0aab3db
83mbyte <registered.mail.mb@gmail.com>
2023-01-25 09:25:40 +0200

    Education section added

--- src/Pages/FormEditPage.js
+++ src/Pages/FormEditPage.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Education from '../Sections/Education';
 import PersonDetails from '../Sections/PersonDetails';
 import ProfSummary from '../Sections/ProfSummary';
 
@@ -12,6 +13,8 @@ const FormEditPage = ({ state, }) => {
 
             {/* 2) Professional Summary section */}
             <ProfSummary title={'Professional Summary'} state={state.data.summary} user={state.loggedUser} />
+            {/* 3) Education section */}
+            <Education title={'Education'} user={state.loggedUser} />
         </>
     );
 };
