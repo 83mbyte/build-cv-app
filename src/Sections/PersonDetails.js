5dff24b50926e1fd0d3f9a3933d32ad6af7348b4
83mbyte <registered.mail.mb@gmail.com>
2023-01-26 17:28:47 +0200

    rebuilt to use redux state

--- src/Sections/PersonDetails.js
+++ src/Sections/PersonDetails.js
@@ -1,34 +1,55 @@
 
-import React, { Fragment } from 'react';
+import React, { Fragment, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import InputCustom from '../components/FormElements/InputCustom';
 import SectionContainer from './SectionContainer';
 
-const PersonDetails = ({ title, state, user }) => {
 
-    //const rootPath = 'personDetails';
+import { fetchPersonDetails } from '../redux/features/personDetails/personDetailsSlice';
+import SpinnerCustom from '../components/Spinner/SpinnerCustom';
+
+
+const PersonDetails = ({ title, user }) => {
+    let content;
+    const dispatch = useDispatch();
+    const data = useSelector(state => state.personDetails.data);
+    const stateStatus = useSelector(state => state.personDetails.status);
+
+    if (stateStatus === 'loading') {
+        content = <SpinnerCustom />
+    }
+    else if (stateStatus === 'succeeded') {
+        content = <Fragment>
+            {
+                Object.keys(data).reverse().map((item, index) => {
+
+
+                    return Object.keys(data[item]).map((i, ind) => {
+
+                        return <InputCustom
+                            key={`${ind}_${index}`}
+                            labelText={data[item][i].label}
+                            defValue={data[item][i].value}
+                            path={data[item][i].path}
+                            required={data[item][i].required}
+                            user={user}
+                        />
+                    })
+                })
+            }
+        </Fragment>
+    }
+
+    useEffect(() => {
+        if (stateStatus === 'idle') {
+            // fetch state
+            dispatch(fetchPersonDetails(user));
+        }
+    }, [stateStatus, dispatch])
+
     return (
         <SectionContainer type={'grid'} headingTxt={title}>
-            <Fragment>
-                {
-                    Object.keys(state).reverse().map((item, index) => {
-
-                        // let basePath = `${item}`;
-                        return Object.keys(state[item]).map((i, ind) => {
-                            // let subPath = `${i}`
-                            // let path = rootPath + '/' + basePath + '/' + subPath + '/value';
-                            return <InputCustom
-                                key={`${ind}_${index}`}
-                                labelText={state[item][i].label}
-                                defValue={state[item][i].value}
-                                path={state[item][i].path}
-                                required={state[item][i].required}
-                                user={user}
-                            />
-                        })
-                    })
-
-                }
-            </Fragment>
+            {content}
         </SectionContainer>
     );
 };
