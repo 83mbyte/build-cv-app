68cee920879c38e2ebb950ef064ac8629c0c34aa
83mbyte <registered.mail.mb@gmail.com>
2023-01-26 17:31:33 +0200

    added inputs to show data

--- src/components/Accordion/AccordionContainer.js
+++ src/components/Accordion/AccordionContainer.js
@@ -1,33 +1,101 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
     Accordion,
     AccordionItem,
     AccordionButton,
     AccordionPanel,
-    AccordionIcon, Box
+    AccordionIcon, Box, SimpleGrid, FormControl, FormLabel, Input, Center, Progress
 } from '@chakra-ui/react';
+import Wysiwyg from '../FormElements/WYSIWYG/Wysiwyg';
+import InputCustom from '../FormElements/InputCustom';
 
-const AccordionContainer = () => {
+const AccordionContainer = ({ children, state, user }) => {
+
+    const [accordionState, setAccordionState] = useState(state);
+
+    const ARRAYSIZE = ['xs', 'md', 'md'];
+    const inputsOrder = ['title', 'degree', 'period', 'location', 'description'];
+
+    const pathCustomize = (prefix, path) => {
+        let pathArr = path.split('/');
+        let res = pathArr.join(`/${prefix}/`)
+        return res
+    }
     return (
-        <Accordion>
-            <AccordionItem>
-                <h2>
-                    <AccordionButton>
-                        <Box as="span" flex='1' textAlign='left'>
-                            Section 1 title
-                        </Box>
-                        <AccordionIcon />
-                    </AccordionButton>
-                </h2>
-                <AccordionPanel pb={4}>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-                    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-                    veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-                    commodo consequat.
-                </AccordionPanel>
-            </AccordionItem>
+        <Box my='3' w={'100%'} px='3px' minW={'200px'}  >
+            <Accordion allowToggle allowMultiple={false}  >
+                {
+                    !accordionState
+                        ? <Center h={'20px'} >
+                            <Box bg='blue' w={['200px', 'xs', 'md', '2xl']}><Progress isIndeterminate size='xs' /></Box>
+                        </Center>
+                        :
+                        accordionState.map((accordItem, index) => {
+                            return (
+                                <AccordionItem border={'1px solid'}
+                                    borderColor={'gray.200'}
+                                    borderRadius={"5px"}
+                                    mb={4}
+                                    key={index}
+                                >
 
-        </Accordion>
+                                    <h2>
+                                        <AccordionButton _hover={{ color: 'teal.400' }} color={'gray.200'}>
+                                            <Box flex={'1'} textAlign='left'>
+                                                <Box color={'black'} _hover={{ color: 'inherit' }} fontSize={['sm', 'sm', 'md']}>{accordItem.title.value}</Box>
+                                                <Box fontSize={'xs'} color={'gray.500'}>{accordItem.period.value}</Box>
+                                            </Box>
+                                            <AccordionIcon />
+                                        </AccordionButton>
+                                    </h2>
+
+                                    <AccordionPanel pb={4}>
+                                        {/* education form */}
+                                        <SimpleGrid columns={[1, 1, 2]}>
+                                            {/* {console.log(state[0])} */}
+                                            {
+                                                inputsOrder &&
+                                                inputsOrder.map((key, ind) => {
+                                                    if (key !== 'description') {
+
+                                                        let path = pathCustomize(index, accordItem[key].path);
+                                                        return (
+                                                            <InputCustom
+                                                                key={`_${ind}`}
+                                                                labelText={accordItem[key].label}
+                                                                defValue={accordItem[key].value}
+                                                                path={path}
+                                                                required={accordItem[key].required}
+                                                                user={user}
+                                                            />
+                                                            // <Box p={2} key={index}>
+                                                            //     <FormControl variant={'floating'} >
+
+                                                            //         <Input size={ARRAYSIZE} placeholder=" " />
+                                                            //         <FormLabel fontSize={ARRAYSIZE}>{state[0][item].label}</FormLabel>
+                                                            //     </FormControl>
+                                                            // </Box>
+                                                        )
+                                                    }
+                                                })
+                                            }
+
+                                        </SimpleGrid>
+                                        {/* <Box p={1}>
+                            <Wysiwyg state={state[0].description} user={user} />
+                        </Box> */}
+                                        {/* education form end */}
+
+
+                                    </AccordionPanel>
+                                </AccordionItem>
+                            )
+                        })
+
+                }
+
+            </Accordion>
+        </Box >
     );
 };
 
