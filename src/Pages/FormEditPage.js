4488f64c7bfdac3b10f53f02f7b4cbf0edb81dcf
83mbyte <registered.mail.mb@gmail.com>
2023-01-26 17:29:51 +0200

    small update

--- src/Pages/FormEditPage.js
+++ src/Pages/FormEditPage.js
@@ -3,18 +3,18 @@ import Education from '../Sections/Education';
 import PersonDetails from '../Sections/PersonDetails';
 import ProfSummary from '../Sections/ProfSummary';
 
-const FormEditPage = ({ state, }) => {
+const FormEditPage = () => {
     return (
         <>
             {/* Sections goes here.. */}
 
             {/* 1) Personal Details section */}
-            <PersonDetails title={'Personal Details'} state={state.data.personDetails} user={state.loggedUser} />
+            <PersonDetails title={'Personal Details'} user={'user_zero'} />
 
             {/* 2) Professional Summary section */}
-            <ProfSummary title={'Professional Summary'} state={state.data.summary} user={state.loggedUser} />
+            {/* <ProfSummary title={'Professional Summary'} state={state.data.summary} user={state.loggedUser} /> */}
             {/* 3) Education section */}
-            <Education title={'Education'} user={state.loggedUser} />
+            {/* <Education title={'Education'} user={state.loggedUser} state={state.data.education} /> */}
         </>
     );
 };
