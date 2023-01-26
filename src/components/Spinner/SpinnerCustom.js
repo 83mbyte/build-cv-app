af2905b322626b1cc7e66aed8ef84baf13039270
83mbyte <registered.mail.mb@gmail.com>
2023-01-26 17:43:29 +0200

    bg changed

--- src/components/Spinner/SpinnerCustom.js
+++ src/components/Spinner/SpinnerCustom.js
@@ -4,7 +4,7 @@ import React from 'react';
 const SpinnerCustom = ({ height = '2rem' }) => {
     return (
 
-        <Center bg='red' h={height}>
+        <Center bg='transparent' h={height}>
             <Box bg='blue' w={['200px', 'xs', 'lg', '3xl']}>
                 <Progress isIndeterminate size='xs' />
             </Box>
