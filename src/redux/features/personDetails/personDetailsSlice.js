ddf8126620d1a1560a13d8c71dd42f731255a291
83mbyte <registered.mail.mb@gmail.com>
2023-01-26 17:26:31 +0200

    personDetailsSlice init commit

---
+++ src/redux/features/personDetails/personDetailsSlice.js
@@ -0,0 +1,112 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { fetchAPI } from "../../../API/api";
+
+export const personDetailsSlice = createSlice({
+    name: 'personDetails',
+    initialState: {
+        data: {
+            positions: {
+                jobTitle: {
+                    label: "Wanted Job Title",
+                    path: "",
+                    type: "text",
+                    value: ""
+                }
+            },
+            name: {
+                firstName: {
+                    label: "First Name",
+                    path: "",
+                    required: "true",
+                    type: "text",
+                    value: "first name"
+                },
+                lastName: {
+                    label: "Last Name",
+                    path: "",
+                    required: "true",
+                    type: "text",
+                    value: "last name"
+                }
+            },
+            address: {
+                city: {
+                    label: "City",
+                    path: "",
+                    type: "text",
+                    value: "your address"
+                },
+                country: {
+                    label: "Country",
+                    path: "",
+                    type: "text",
+                    value: "your country"
+                },
+                street: {
+                    label: "Street",
+                    path: "",
+                    type: "text",
+                    value: "your street"
+                }
+            },
+            contacts: {
+                email: {
+                    label: "Email",
+                    path: "",
+                    required: "true",
+                    type: "text",
+                    value: "example@example.com"
+                },
+                phone: {
+                    label: "Phone",
+                    path: "",
+                    required: "false",
+                    type: "text",
+                    value: "+35989878597"
+                },
+
+            },
+        },
+        status: "idle",
+        error: null
+
+    },
+    reducers: {
+        loadStateFrom: (state, action) => {
+            state.data = action.payload
+        },
+        setJobTitle: state => {
+            state.data.positions.jobTitle.value = 'Test Set JobTitle'
+        }
+    },
+    extraReducers(builder) {
+        builder
+            .addCase(fetchPersonDetails.pending, (state, action) => {
+                state.status = 'loading'
+            })
+            .addCase(fetchPersonDetails.fulfilled, (state, action) => {
+                state.status = 'succeeded';
+                state.data = action.payload;
+            })
+            .addCase(fetchPersonDetails.rejected, (state, action) => {
+                state.status = 'failed'
+                state.error = action.error.message
+            })
+    }
+})
+
+export const { setJobTitle, loadStateFrom } = personDetailsSlice.actions;
+
+export default personDetailsSlice.reducer;
+
+export const fetchPersonDetails = createAsyncThunk('personDetails/fetchPersonDetails', async (userName) => {
+    const data = await fetchAPI.fethingSubPath('personDetails', userName)
+
+    // if (data && data !== 'Error -- simpleFetchData from api.js') {
+    //     dispatch(loadStateFrom(data.personDetails) || null);
+    //     console.log(loadStateFrom(data.personDetails))
+    // }
+    return data
+})
+
+
