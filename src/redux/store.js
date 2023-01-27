c1fb164854069a121fc667c49a22c8710ec7c291
83mbyte <registered.mail.mb@gmail.com>
2023-01-27 15:52:53 +0200

    getDefaultMiddleware added but commented

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,4 +1,4 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
 import personDetailsReducer from "./features/personDetails/personDetailsSlice";
 import summarySliceReducer from "./features/summary/summarySlice";
 
@@ -6,5 +6,14 @@ export default configureStore({
     reducer: {
         personDetails: personDetailsReducer,
         summary: summarySliceReducer
-    }
+    },
+    // middleware: (getDefaultMiddleware) => getDefaultMiddleware({
+    //     // serializableCheck: false
+    //     serializableCheck: {
+    //         //     ignoredActions: ['summary/putDataSummary/fulfilled'],
+    //         ignoredActionPaths: ['meta']
+    //     }
+
+    //     // ignoredActionPaths: ['meta.arg', 'payload.timestamp']
+    // })
 })
