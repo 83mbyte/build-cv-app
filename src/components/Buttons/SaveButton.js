bc5ce49e36122070ba14dc588f53f220db0320de
83mbyte <registered.mail.mb@gmail.com>
2023-01-30 16:01:03 +0200

    style changes

--- src/components/Buttons/SaveButton.js
+++ src/components/Buttons/SaveButton.js
@@ -5,7 +5,7 @@ const SaveButton = ({ saveToServer, buttonStatus }) => {
     return (
         <Box bg='transparent' textAlign={['center', 'right', 'right']} px={2}>
             <Button
-                _hover={{ backgroundColor: 'teal.300', color: 'white' }}
+                // _hover={{ backgroundColor: 'teal.300', color: 'white' }}
                 colorScheme={'teal'} size={['xs', 'xs', 'sm']}
                 margin={['8px auto 3px auto', '8px auto 3px auto', '15px auto 5px auto']}
                 isLoading={buttonStatus == 'loading'}
--- src/components/FormElements/InputCustom.js
+++ src/components/FormElements/InputCustom.js
@@ -20,6 +20,8 @@ const InputCustom = ({ labelText, defValue, path, required = false, user, handle
                     //onBlur={applyChanges}
                     bg="white"
                     placeholder=" "
+                    onFocus={() => console.log('focu')}
+                    _focusVisible={{ 'boxShadow': 'none' }}
                 />
                 <FormLabel fontSize={ARRAYSIZE}>{labelText}</FormLabel>
                 {isError && (
