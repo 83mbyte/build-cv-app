00cbcdd497cd292f112a8a179990778f18c32158
83mbyte <registered.mail.mb@gmail.com>
2023-01-31 17:42:52 +0200

    SlideBottom added

--- src/Pages/FormEditPage.js
+++ src/Pages/FormEditPage.js
@@ -1,9 +1,15 @@
+
 import React from 'react';
+import { useSelector } from 'react-redux';
+import SlideBottom from '../components/Slide/SlideBottom';
 import Education from '../Sections/Education';
 import PersonDetails from '../Sections/PersonDetails';
 import ProfSummary from '../Sections/ProfSummary';
+import SaveDataServ from '../Sections/SaveDataServ';
 
 const FormEditPage = () => {
+    const isModified = useSelector(state => state.utility.data.isModified);
+
     return (
         <>
             {/* Sections goes here.. */}
@@ -15,8 +21,12 @@ const FormEditPage = () => {
             <ProfSummary title={'Professional Summary'} user={'user_zero'} />
             {/* 3) Education section */}
             <Education title={'Education'} user={'user_zero'} />
+            <SlideBottom show={isModified.status}><SaveDataServ user={'user_hero'} sections={isModified.sections} /></SlideBottom>
+
+
         </>
     );
 };
 
 export default FormEditPage;
+
