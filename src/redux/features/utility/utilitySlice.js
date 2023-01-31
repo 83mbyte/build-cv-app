0364d0147d78c9e9d1d6f517eae4948ceab56f03
83mbyte <registered.mail.mb@gmail.com>
2023-01-31 18:20:49 +0200

    AsynkThunk to put data on server

--- src/redux/features/utility/utilitySlice.js
+++ src/redux/features/utility/utilitySlice.js
@@ -1,4 +1,5 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { fetchAPI } from "../../../API/api";
 
 const UTILITY_DATA_TEMPLATE = {
     isModified: {
@@ -20,13 +21,33 @@ const utilitySlice = createSlice({
                 state.data.isModified.sections = [...state.data.isModified.sections, action.payload.section];
             }
         },
-        setIsModifiedFalse: (state,) => {
+        setIsModifiedFalse: (state) => {
             state.data.isModified.status = false;
             state.data.isModified.sections = [];
         }
 
+    },
+    extraReducers(builder) {
+        builder
+            .addCase(putDataOnServer.fulfilled, (state, action) => {
+                let index = state.data.isModified.sections.indexOf(action.payload);
+                if (index !== -1) {
+                    state.data.isModified.sections.splice(index, 1);
+                    if (state.data.isModified.sections.length == 0) {
+                        state.data.isModified.status = false;
+                    }
+                }
+            })
     }
+
 })
 
 export const { setIsModifiedTrue, setIsModifiedFalse } = utilitySlice.actions;
 export default utilitySlice.reducer;
+
+export const putDataOnServer = createAsyncThunk('utility/putDataOnServer', async (data) => {
+    const response = await fetchAPI.putDataToWholeSection(data.user, data.path, data.value);
+    if (response.ok && response.status === 200) {
+        return data.path
+    }
+})
