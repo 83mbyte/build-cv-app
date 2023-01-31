59adf52e4900cb9903a5714d00b27f222cbfd367
83mbyte <registered.mail.mb@gmail.com>
2023-01-31 17:43:13 +0200

    utilitySliceReducer added

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,12 +2,14 @@ import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
 import educationSliceReducer from "./features/education/educationSlice";
 import personDetailsReducer from "./features/personDetails/personDetailsSlice";
 import summarySliceReducer from "./features/summary/summarySlice";
+import utilitySliceReducer from "./features/utility/utilitySlice";
 
 export default configureStore({
     reducer: {
         personDetails: personDetailsReducer,
         summary: summarySliceReducer,
-        education: educationSliceReducer
+        education: educationSliceReducer,
+        utility: utilitySliceReducer
     },
     // middleware: (getDefaultMiddleware) => getDefaultMiddleware({
     //     // serializableCheck: false
