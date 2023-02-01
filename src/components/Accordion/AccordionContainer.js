d230dac45a0fe07d3096178274052f785e090753
83mbyte <registered.mail.mb@gmail.com>
2023-02-01 13:04:00 +0200

    isExpanded state use for rendering icons

--- src/components/Accordion/AccordionContainer.js
+++ src/components/Accordion/AccordionContainer.js
@@ -4,19 +4,18 @@ import {
     AccordionItem,
     AccordionButton,
     AccordionPanel,
-    AccordionIcon, Box, SimpleGrid, Center, Progress, Button, Flex, IconButton
+    Box, SimpleGrid, Flex,
 } from '@chakra-ui/react';
 import Wysiwyg from '../FormElements/WYSIWYG/Wysiwyg';
 import InputCustom from '../FormElements/InputCustom';
 import SpinnerCustom from '../Spinner/SpinnerCustom';
 import SaveButton from '../Buttons/SaveButton';
-import { RemoveIcon } from '../Icons/Icon';
+import { ExpandIcon, RemoveIcon } from '../Icons/Icon';
 import ToolTip from '../ToolTip/ToolTip';
 
 const AccordionContainer = ({ state, user, handleEditorChange, handleInputChange, buttonStatus, saveToServer, removeItem }) => {
 
 
-    const ARRAYSIZE = ['xs', 'md', 'md'];
     const inputsOrder = ['title', 'degree', 'period', 'location', 'description'];
 
     const pathCustomize = (prefix, path) => {
@@ -40,75 +39,84 @@ const AccordionContainer = ({ state, user, handleEditorChange, handleInputChange
                                     mb={4}
                                     key={index}
                                 >
-                                    <h2>
-                                        <AccordionButton _hover={{ color: 'teal.400' }} color={'gray.200'}>
-                                            <Box flex={'1'} textAlign='left'>
-                                                <Box color={'black'} _hover={{ color: 'inherit' }} fontSize={['sm', 'sm', 'md']}>
-                                                    {
-                                                        (!accordItem.title.value || accordItem.title.value === '' || accordItem.title.value === undefined)
-                                                            ? `(Not specified)`
-                                                            : accordItem.title.value
-                                                    }
-                                                </Box>
+                                    {
+                                        ({ isExpanded }) => (
+                                            <>
+                                                <h2>
+                                                    <AccordionButton _hover={{ color: 'teal.500' }} color={isExpanded ? 'teal.500' : 'gray.200'}>
+                                                        <Box flex={'1'} textAlign='left'>
+                                                            <Box color={'black'} _hover={{ color: 'inherit' }} fontSize={['sm', 'sm', 'md']}>
+                                                                {
+                                                                    (!accordItem.title.value || accordItem.title.value === '' || accordItem.title.value === undefined)
+                                                                        ? `(Not specified)`
+                                                                        : accordItem.title.value
+                                                                }
+                                                            </Box>
 
-                                                <Box fontSize={'xs'} color={'gray.500'}>
-                                                    {
-                                                        accordItem.period.value !== 'xxxx - yyyy' && accordItem.period.value
-                                                    }
-                                                </Box>
-                                            </Box>
-                                            <Flex columnGap={'15px'} alignItems={'center'} mr='15px'>
+                                                            <Box fontSize={'xs'} color={'gray.500'}>
+                                                                {
+                                                                    accordItem.period.value !== 'xxxx - yyyy' && accordItem.period.value
+                                                                }
+                                                            </Box>
+                                                        </Box>
+                                                        <Flex columnGap={'15px'} alignItems={'center'} mr='15px'>
 
-                                                <Box onClick={(e) => removeItem(e, state[index])}>
-                                                    <ToolTip label='remove' type='warning'>
-                                                        <RemoveIcon />
-                                                    </ToolTip>
-                                                </Box>
-                                            </Flex>
+                                                            <Box onClick={(e) => removeItem(e, state[index])}>
+                                                                <ToolTip label='remove' type='warning'>
+                                                                    <RemoveIcon isExpanded={isExpanded} />
+                                                                </ToolTip>
+                                                            </Box>
+                                                        </Flex>
 
-                                            <AccordionIcon mx={1} />
-                                        </AccordionButton>
-                                    </h2>
+                                                        {/* {isExpanded && <AccordionIcon mx={1} />} */}
+                                                        <ToolTip label={isExpanded ? 'show less' : 'show more'}>
+                                                            <ExpandIcon isExpanded={isExpanded} />
+                                                        </ToolTip>
+                                                    </AccordionButton>
+                                                </h2>
 
-                                    <AccordionPanel pb={4}>
-                                        {/* education form */}
-                                        <SimpleGrid columns={[1, 1, 2]}>
+                                                <AccordionPanel pb={4}>
+                                                    {/* education form */}
+                                                    <SimpleGrid columns={[1, 1, 2]}>
 
-                                            {
-                                                inputsOrder &&
-                                                inputsOrder.map((key, ind) => {
-                                                    if (key !== 'description') {
+                                                        {
+                                                            inputsOrder &&
+                                                            inputsOrder.map((key, ind) => {
+                                                                if (key !== 'description') {
 
-                                                        let path = pathCustomize(index, accordItem[key].path);
-                                                        return (
-                                                            <InputCustom
-                                                                key={`${index}_${ind}`}
-                                                                labelText={accordItem[key].label}
-                                                                defValue={accordItem[key].value}
-                                                                path={path}
-                                                                required={accordItem[key].required}
-                                                                user={user}
-                                                                handleInputChange={handleInputChange}
-                                                            />
+                                                                    let path = pathCustomize(index, accordItem[key].path);
+                                                                    return (
+                                                                        <InputCustom
+                                                                            key={`${index}_${ind}`}
+                                                                            labelText={accordItem[key].label}
+                                                                            defValue={accordItem[key].value}
+                                                                            path={path}
+                                                                            required={accordItem[key].required}
+                                                                            user={user}
+                                                                            handleInputChange={handleInputChange}
+                                                                        />
 
-                                                        )
-                                                    }
-                                                })
-                                            }
+                                                                    )
+                                                                }
+                                                                return null
+                                                            })
+                                                        }
 
-                                        </SimpleGrid>
-                                        <Box  >
+                                                    </SimpleGrid>
+                                                    <Box  >
 
-                                            <Wysiwyg state={state[index].description} user={user} path={pathCustomize(index, state[index].description.path)} handleEditorChange={handleEditorChange} />
-                                        </Box>
-                                        <Box>
+                                                        <Wysiwyg state={state[index].description} user={user} path={pathCustomize(index, state[index].description.path)} handleEditorChange={handleEditorChange} />
+                                                    </Box>
+                                                    {/* <Box>
                                             <SaveButton saveToServer={saveToServer} buttonStatus={buttonStatus} />
-                                        </Box>
+                                        </Box> */}
 
-                                        {/* education form end */}
+                                                    {/* education form end */}
 
 
-                                    </AccordionPanel>
+                                                </AccordionPanel></>
+                                        )
+                                    }
                                 </AccordionItem>
                             )
                         })
