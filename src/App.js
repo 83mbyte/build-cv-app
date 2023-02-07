539dc5761855491c0f501b81cc7d237138b5e094
83mbyte <registered.mail.mb@gmail.com>
2023-02-07 12:02:30 +0200

    margin styles setted

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { Box } from '@chakra-ui/react';
 import React from 'react';
 import './App.css';
 import FormEditPage from './Pages/FormEditPage';
@@ -5,9 +6,9 @@ import FormEditPage from './Pages/FormEditPage';
 function App() {
 
   return (
-    <>
+    <Box m={'10px auto 30px auto'}>
       <FormEditPage />
-    </>
+    </Box>
   );
 }
 
