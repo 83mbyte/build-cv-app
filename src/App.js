312f25c1f50850dcc2b52b68e56901338c683dde
83mbyte <registered.mail.mb@gmail.com>
2023-02-08 14:29:07 +0200

    Login page added

--- src/App.js
+++ src/App.js
@@ -3,11 +3,20 @@ import React from 'react';
 import './App.css';
 import FormEditPage from './Pages/FormEditPage';
 
+import { useSelector } from 'react-redux';
+import Login from './Pages/Login';
 function App() {
 
+  const loggedUser = useSelector(state => state.utility.auth.data);
+
   return (
-    <Box m={'10px auto 30px auto'}>
-      <FormEditPage />
+    <Box m={'10px auto 30px auto'} w='full'>
+
+      {
+        loggedUser && loggedUser.userId && loggedUser.accessToken
+          ? <FormEditPage />
+          : <Login />
+      }
     </Box>
   );
 }
