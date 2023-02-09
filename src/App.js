864ef56d344726b01fc4a131009ac70fcb185eb6
83mbyte <registered.mail.mb@gmail.com>
2023-02-09 12:31:32 +0200

    Register renamed to SignUp component

--- src/App.js
+++ src/App.js
@@ -4,19 +4,26 @@ import './App.css';
 import FormEditPage from './Pages/FormEditPage';
 
 import { useSelector } from 'react-redux';
+import SignUp from './Pages/SignUp';
 import Login from './Pages/Login';
+
 function App() {
 
   const loggedUser = useSelector(state => state.utility.auth.data);
 
   return (
-    <Box m={'10px auto 30px auto'} w='full'>
 
-      {
+    <Box m={'10px auto 30px auto'} w='full'>
+      <FormEditPage />
+      {/* <FormEditPage /> */}
+      {/* Login page disabled */}
+      {/* {
         loggedUser && loggedUser.userId && loggedUser.accessToken
           ? <FormEditPage />
           : <Login />
-      }
+      } */}
+      {/* <SignUp /> */}
+      {/* <Login /> */}
     </Box>
   );
 }
