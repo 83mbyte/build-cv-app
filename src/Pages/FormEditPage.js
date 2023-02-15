50997c7022e885da011f0cd239f177bebaec5f08
83mbyte <registered.mail.mb@gmail.com>
2023-02-15 17:49:20 +0200

    SlideBottom, SaveDataModal components removed

--- src/Pages/FormEditPage.js
+++ src/Pages/FormEditPage.js
@@ -1,7 +1,7 @@
 
+import { Box } from '@chakra-ui/react';
 import React from 'react';
-import { useSelector } from 'react-redux';
-import SlideBottom from '../components/Slide/SlideBottom';
+
 import Courses from '../Sections/Courses';
 import Education from '../Sections/Education';
 import EmploymentHistory from '../Sections/EmploymentHistory';
@@ -10,53 +10,50 @@ import Languages from '../Sections/Languages';
 import PersonDetails from '../Sections/PersonDetails';
 import ProfSummary from '../Sections/ProfSummary';
 import References from '../Sections/References';
-import SaveDataModal from '../Sections/SaveDataModal';
+
 import Skills from '../Sections/Skills';
 import WebSocLinks from '../Sections/WebSocLinks';
 
 const FormEditPage = () => {
-    const isModified = useSelector(state => state.utility.isModifiedContent);
+
+    let loggedUser = 'user_hero'; //must be replaced to logged user from state..
 
     return (
-        <>
+
+        <Box pt={10} >
             {/* Sections goes here.. */}
 
             {/* 1) Personal Details section */}
-            <PersonDetails title={'Personal Details'} user={'user_hero'} />
+            <PersonDetails title={'Personal Details'} user={loggedUser} />
 
             {/* 2) Professional Summary section */}
-            <ProfSummary title={'Professional Summary'} user={'user_hero'} />
+            <ProfSummary title={'Professional Summary'} user={loggedUser} />
 
             {/* 3) Education section */}
-            <Education title={'Education'} user={'user_hero'} />
+            <Education title={'Education'} user={loggedUser} />
 
             {/* 4) Web & Social Links section */}
-            <WebSocLinks title={'Websites & Social Links'} user={'user_hero'} />
+            <WebSocLinks title={'Websites & Social Links'} user={loggedUser} />
 
             {/* 5) Skills section */}
-            <Skills title={'Skills'} user={'user_hero'} />
+            <Skills title={'Skills'} user={loggedUser} />
 
             {/* 6) Courses section */}
-            <Courses title={'Courses'} user={'user_hero'} />
+            <Courses title={'Courses'} user={loggedUser} />
 
             {/* 7) Employment History section */}
-            <EmploymentHistory title={'Employment History'} user={'user_hero'} />
+            <EmploymentHistory title={'Employment History'} user={loggedUser} />
 
             {/* 8) Languages section */}
-            <Languages title={'Languages'} user={'user_hero'} />
+            <Languages title={'Languages'} user={loggedUser} />
 
             {/* 9) References section */}
-            <References title={'References'} user={'user_hero'} />
+            <References title={'References'} user={loggedUser} />
 
             {/* 10) Hobbies section */}
-            <Hobbies title={"Hobbies"} user={'user_hero'} />
+            <Hobbies title={"Hobbies"} user={loggedUser} />
 
-
-            {/*   popup/slide_bottom Save button */}
-            <SlideBottom show={isModified.status}><SaveDataModal user={'user_hero'} sections={isModified.sections} /></SlideBottom>
-
-
-        </>
+        </Box>
     );
 };
 
