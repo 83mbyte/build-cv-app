a9e18471a1d38f177c35b0ffa35e9c7cfd4b599f
83mbyte <registered.mail.mb@gmail.com>
2023-02-20 19:03:19 +0200

    isDisabled option added

--- src/components/ToolTip/ToolTip.js
+++ src/components/ToolTip/ToolTip.js
@@ -1,13 +1,13 @@
 import { Box, Tooltip } from '@chakra-ui/react';
 import React, { forwardRef } from 'react';
 
-const ToolTip = ({ children, label, type = 'default' }) => {
+const ToolTip = ({ children, label, type = 'default', isDisabled }) => {
     const colors = {
         default: 'teal',
         warning: 'red',
     }
     return (
-        <Tooltip label={label} hasArrow bg={`${colors[type]}.600`} placement='top-end' >
+        <Tooltip label={label} hasArrow bg={`${colors[type]}.600`} placement='top-end' closeOnClick={true} isDisabled={isDisabled} >
             <TooltipItem>{children}</TooltipItem>
         </Tooltip>
     );
