cc05b574a81aded4f382b5733d9baa2d5cfd0a86
83mbyte <registered.mail.mb@gmail.com>
2023-03-01 15:30:09 +0200

    React.lazy() - lazy loading for sections components

--- src/Pages/FormEditPage.js
+++ src/Pages/FormEditPage.js
@@ -1,18 +1,27 @@
 
 import { Box } from '@chakra-ui/react';
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
+import SpinnerCustom from '../components/Spinner/SpinnerCustom';
 
-import Courses from '../Sections/Courses';
-import Education from '../Sections/Education';
-import EmploymentHistory from '../Sections/EmploymentHistory';
-import Hobbies from '../Sections/Hobbies';
-import Languages from '../Sections/Languages';
-import PersonDetails from '../Sections/PersonDetails';
-import ProfSummary from '../Sections/ProfSummary';
-import References from '../Sections/References';
+const Courses = lazy(() => import('../Sections/Courses'));
 
-import Skills from '../Sections/Skills';
-import WebSocLinks from '../Sections/WebSocLinks';
+const Education = lazy(() => import('../Sections/Education'));
+
+const EmploymentHistory = lazy(() => import('../Sections/EmploymentHistory'))
+
+const Hobbies = lazy(() => import('../Sections/Hobbies'));
+
+const Languages = lazy(() => import('../Sections/Languages'));
+
+const PersonDetails = lazy(() => import('../Sections/PersonDetails'));
+
+const ProfSummary = lazy(() => import('../Sections/ProfSummary'));
+
+const References = lazy(() => import('../Sections/References'));
+
+const Skills = lazy(() => import('../Sections/Skills'))
+
+const WebSocLinks = lazy(() => import('../Sections/WebSocLinks'));
 
 const FormEditPage = ({ user }) => {
 
@@ -20,38 +29,38 @@ const FormEditPage = ({ user }) => {
     return (
 
         <Box pt={10} >
-            {/* Sections goes here.. */}
+            {/* Sections.. */}
+            <Suspense fallback={<SpinnerCustom />}>
+                {/* 1) Personal Details section */}
+                <PersonDetails title={'Personal Details'} user={user} />
 
-            {/* 1) Personal Details section */}
-            <PersonDetails title={'Personal Details'} user={user} />
+                {/* 2) Professional Summary section */}
+                <ProfSummary title={'Professional Summary'} user={user} />
 
-            {/* 2) Professional Summary section */}
-            <ProfSummary title={'Professional Summary'} user={user} />
+                {/* 3) Education section */}
+                <Education title={'Education'} user={user} />
 
-            {/* 3) Education section */}
-            <Education title={'Education'} user={user} />
+                {/* 4) Web & Social Links section */}
+                <WebSocLinks title={'Websites & Social Links'} user={user} />
 
-            {/* 4) Web & Social Links section */}
-            <WebSocLinks title={'Websites & Social Links'} user={user} />
+                {/* 5) Skills section */}
+                <Skills title={'Skills'} user={user} />
 
-            {/* 5) Skills section */}
-            <Skills title={'Skills'} user={user} />
+                {/* 6) Courses section */}
+                <Courses title={'Courses'} user={user} />
 
-            {/* 6) Courses section */}
-            <Courses title={'Courses'} user={user} />
+                {/* 7) Employment History section */}
+                <EmploymentHistory title={'Employment History'} user={user} />
 
-            {/* 7) Employment History section */}
-            <EmploymentHistory title={'Employment History'} user={user} />
+                {/* 8) Languages section */}
+                <Languages title={'Languages'} user={user} />
 
-            {/* 8) Languages section */}
-            <Languages title={'Languages'} user={user} />
+                {/* 9) References section */}
+                <References title={'References'} user={user} />
 
-            {/* 9) References section */}
-            <References title={'References'} user={user} />
-
-            {/* 10) Hobbies section */}
-            <Hobbies title={"Hobbies"} user={user} />
-
+                {/* 10) Hobbies section */}
+                <Hobbies title={"Hobbies"} user={user} />
+            </Suspense>
         </Box>
     );
 };
