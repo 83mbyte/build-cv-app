0fc8b9118be49a0cb8d4d3efb83c3e476c63faa7
83mbyte <registered.mail.mb@gmail.com>
2023-03-07 11:58:41 +0200

    update of Suspense components structure

--- src/Pages/FormEditPage.js
+++ src/Pages/FormEditPage.js
@@ -33,22 +33,32 @@ const FormEditPage = ({ user }) => {
             <Suspense fallback={<SpinnerCustom />}>
                 {/* 1) Personal Details section */}
                 <PersonDetails title={'Personal Details'} user={user} />
+            </Suspense>
 
+            <Suspense fallback={<SpinnerCustom />}>
                 {/* 2) Professional Summary section */}
                 <ProfSummary title={'Professional Summary'} user={user} />
+            </Suspense>
 
+            <Suspense fallback={<SpinnerCustom />}>
                 {/* 3) Education section */}
                 <Education title={'Education'} user={user} />
+            </Suspense>
 
+            <Suspense fallback={<SpinnerCustom />}>
                 {/* 4) Web & Social Links section */}
                 <WebSocLinks title={'Websites & Social Links'} user={user} />
-
+            </Suspense>
+            <Suspense fallback={<SpinnerCustom />}>
                 {/* 5) Skills section */}
                 <Skills title={'Skills'} user={user} />
-
+            </Suspense>
+            <Suspense fallback={<SpinnerCustom />}>
                 {/* 6) Courses section */}
                 <Courses title={'Courses'} user={user} />
+            </Suspense>
 
+            <Suspense fallback={<SpinnerCustom />}>
                 {/* 7) Employment History section */}
                 <EmploymentHistory title={'Employment History'} user={user} />
 
@@ -61,7 +71,7 @@ const FormEditPage = ({ user }) => {
                 {/* 10) Hobbies section */}
                 <Hobbies title={"Hobbies"} user={user} />
             </Suspense>
-        </Box>
+        </Box >
     );
 };
 
