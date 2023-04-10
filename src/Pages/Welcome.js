93ade3b89e604b6a1dba2f54ce3c79d61f23983d
83mbyte <registered.mail.mb@gmail.com>
2023-04-10 13:30:00 +0300

    WelcomeBlockImage component removed from Welcome.

--- src/Pages/Welcome.js
+++ src/Pages/Welcome.js
@@ -1,9 +1,9 @@
-import { Box, VStack, } from '@chakra-ui/react';
+import { VStack, } from '@chakra-ui/react';
 import React from 'react';
 
 import WelcomeTopElement from '../components/WelcomeComponents/WelcomeTopElement';
-import WelcomeBlockImage from '../components/WelcomeComponents/WelcomeBlockImage';
 import WelcomeFeatures from '../components/WelcomeComponents/WelcomeFeatures';
+import WelcomeFAQ from '../components/WelcomeComponents/WelcomeFAQ';
 
 const Welcome = () => {
 
@@ -12,9 +12,8 @@ const Welcome = () => {
     return (
         <VStack w={'full'} spacing={0} borderColor={'gray.300'}  >
             <WelcomeTopElement />
-
-            {/* <WelcomeBlockImage /> */}
             <WelcomeFeatures />
+            <WelcomeFAQ />
         </VStack >
 
     );
