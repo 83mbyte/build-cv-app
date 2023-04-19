e8c51d547b6269381cd7260ba67a2be00dc2ceb1
83mbyte <registered.mail.mb@gmail.com>
2023-04-19 18:17:14 +0300

    divider colors were modified

--- src/components/Divider/DividerCustom.js
+++ src/components/Divider/DividerCustom.js
@@ -4,18 +4,18 @@ import React from 'react';
 const DividerCustom = () => {
     return (
 
-        <VStack spacing={0.5} mt={0} mb={3} border={'0px solid red'}>
+        <VStack spacing={0.5} mt={5} mb={0} border={'0px solid red'}>
             <HStack spacing={8} bg='' w="75%" mx={'auto'}>
-                <Box bg={'gray.100'} w={'full'}><Divider /></Box>
-                <Box bg={'gray.100'} w={'full'}><Divider /></Box>
+                <Box bg={'teal.50'} w={'full'}><Divider /></Box>
+                <Box bg={'teal.50'} w={'full'}><Divider /></Box>
             </HStack>
             <HStack spacing={6} bg='' w="55%" mx={'auto'}>
-                <Box bg={'gray.50'} w={'full'}><Divider /></Box>
-                <Box bg={'gray.50'} w={'full'}><Divider /></Box>
+                <Box bg={'teal.100'} w={'full'}><Divider /></Box>
+                <Box bg={'teal.100'} w={'full'}><Divider /></Box>
             </HStack>
             <HStack spacing={4} bg='' w="35%" mx={'auto'}>
-                <Box bg={'gray.50'} w={'full'}><Divider /></Box>
-                <Box bg={'gray.50'} w={'full'}><Divider /></Box>
+                <Box bg={'teal.50'} w={'full'}><Divider /></Box>
+                <Box bg={'teal.50'} w={'full'}><Divider /></Box>
             </HStack>
         </VStack>
     );
