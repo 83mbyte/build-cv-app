3e84d15f676854c80f7982f6adc6a764f3a17b25
83mbyte <registered.mail.mb@gmail.com>
2023-04-20 17:58:17 +0300

    codesplitting (lazy loading) for additional sections

--- src/Pages/Dashboard.js
+++ src/Pages/Dashboard.js
@@ -1,6 +1,6 @@
 
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React, { Fragment, lazy, Suspense, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import LayoutDashboard from '../components/Layouts/LayoutDashboard';
 import { Navigate } from 'react-router-dom';
 import PersonDetails from '../components/Sections/PersonDetails';
@@ -8,14 +8,32 @@ import Summary from '../components/Sections/Summary';
 import Education from '../components/Sections/Education';
 import Links from '../components/Sections/Links';
 import Skills from '../components/Sections/Skills';
-import Courses from '../components/Sections/Courses';
 import History from '../components/Sections/History';
-import Languages from '../components/Sections/Languages';
-import References from '../components/Sections/References';
-import Hobbies from '../components/Sections/Hobbies';
+import { getAdditionalSections } from '../redux/features/utility/utilitySlice';
+
+const Hobbies = lazy(() => import('../components/Sections/Hobbies'));
+const Languages = lazy(() => import('../components/Sections/Languages'));
+const Courses = lazy(() => import('../components/Sections/Courses'));
+const References = lazy(() => import('../components/Sections/References'));
 
 const Dashboard = () => {
-    const loggedUser = useSelector(state => state.utility.auth.data)
+    const loggedUser = useSelector(state => state.utility.auth.data);
+    const additionalSections = useSelector(state => state.utility.additionalSections);
+    const dispatch = useDispatch();
+
+    const sectionsToShow = {
+        hobbies: <Hobbies loggedUser={loggedUser} key={'section_hobbies'} />,
+        courses: <Courses loggedUser={loggedUser} key={'section_curses'} />,
+        references: <References loggedUser={loggedUser} key={'section_references'} />,
+        languages: <Languages loggedUser={loggedUser} key={'section_languages'} />
+    }
+
+    useEffect(() => {
+        if (additionalSections.status === 'idle') {
+            dispatch(getAdditionalSections(loggedUser));
+        }
+    }, [additionalSections.status, loggedUser, dispatch])
+
     return (
         <>
             {
@@ -45,22 +63,23 @@ const Dashboard = () => {
                         {/* Employment History */}
                         <History loggedUser={loggedUser} />
 
-                        {/* Courses */}
-                        <Courses loggedUser={loggedUser} />
 
+                        {
+                            additionalSections.data
+                            && additionalSections.data.map((item, index) => {
 
-                        {/* Languages */}
-                        <Languages loggedUser={loggedUser} />
+                                return (
+                                    // Additional sections  
+                                    <Fragment key={index}>
+                                        <Suspense >
+                                            {sectionsToShow[item]}
+                                        </Suspense>
+                                    </Fragment>
+                                )
+                            })
+                        }
 
-
-                        {/* References */}
-                        <References loggedUser={loggedUser} />
-
-
-                        {/* Hobbies */}
-                        <Hobbies loggedUser={loggedUser} />
-
-                    </LayoutDashboard>
+                    </LayoutDashboard >
                     : <Navigate to={'/login'} />
             }
         </>
