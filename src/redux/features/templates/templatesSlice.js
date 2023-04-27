0b66497530831b435c5871c3accfbea1ec029406
83mbyte <registered.mail.mb@gmail.com>
2023-04-27 17:31:51 +0300

    added some comments

--- src/redux/features/templates/templatesSlice.js
+++ src/redux/features/templates/templatesSlice.js
@@ -9,10 +9,10 @@ const templatesSlice = createSlice({
                 label: 'Amsterdam',
                 img: 'https://s3.resume.io/cdn-cgi/image/format=auto,fit=scale-down,dpr=1,width=154/uploads/local_template_image/image/503/persistent-resource/amsterdam-resume-templates.jpg'
             },
-            {
-                label: 'Dublin',
-                img: 'https://s3.resume.io/cdn-cgi/image/format=auto,fit=scale-down,dpr=1,width=154/uploads/local_template_image/image/488/persistent-resource/dublin-resume-templates.jpg'
-            },
+            // {
+            //     label: 'Dublin',
+            //     img: 'https://s3.resume.io/cdn-cgi/image/format=auto,fit=scale-down,dpr=1,width=154/uploads/local_template_image/image/488/persistent-resource/dublin-resume-templates.jpg'
+            // },
             {
                 label: 'Vivien',
                 img: 'https://s3.resume.io/cdn-cgi/image/format=auto,fit=scale-down,dpr=1,width=154/uploads/local_template_image/image/406/persistent-resource/vienna-resume-templates.jpg'
