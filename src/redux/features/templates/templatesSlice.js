d8a459d392c9859a9f2e6a635d49106de78fe32a
83mbyte <registered.mail.mb@gmail.com>
2023-05-02 16:39:11 +0300

    disclaimer added as a comment

--- src/redux/features/templates/templatesSlice.js
+++ src/redux/features/templates/templatesSlice.js
@@ -5,22 +5,28 @@ const templatesSlice = createSlice({
     initialState: {
         data: {
             selected: null,
-            variants: [{
-                label: 'Amsterdam',
-                img: 'https://s3.resume.io/cdn-cgi/image/format=auto,fit=scale-down,dpr=1,width=154/uploads/local_template_image/image/503/persistent-resource/amsterdam-resume-templates.jpg'
-            },
-            // {
-            //     label: 'Dublin',
-            //     img: 'https://s3.resume.io/cdn-cgi/image/format=auto,fit=scale-down,dpr=1,width=154/uploads/local_template_image/image/488/persistent-resource/dublin-resume-templates.jpg'
-            // },
-            {
-                label: 'Vivien',
-                img: 'https://s3.resume.io/cdn-cgi/image/format=auto,fit=scale-down,dpr=1,width=154/uploads/local_template_image/image/406/persistent-resource/vienna-resume-templates.jpg'
-            },
-            {
-                label: 'Lndn',
-                img: 'https://s3.resume.io/cdn-cgi/image/format=auto,fit=scale-down,dpr=1,width=154/uploads/local_template_image/image/481/persistent-resource/london-resume-templates.jpg'
-            },
+            variants: [
+
+                ///////////////////// DISCLAIMER ///////////////////
+                // all the provided pictures are being used for   // 
+                // the demonstration purpose only.              //
+                /////////////////////////////////////////////////
+                {
+                    label: 'Amsterdam',
+                    img: 'https://s3.resume.io/cdn-cgi/image/format=auto,fit=scale-down,dpr=1,width=154/uploads/local_template_image/image/503/persistent-resource/amsterdam-resume-templates.jpg'
+                },
+                // {
+                //     label: 'Dublin',
+                //     img: 'https://s3.resume.io/cdn-cgi/image/format=auto,fit=scale-down,dpr=1,width=154/uploads/local_template_image/image/488/persistent-resource/dublin-resume-templates.jpg'
+                // },
+                {
+                    label: 'Vivien',
+                    img: 'https://s3.resume.io/cdn-cgi/image/format=auto,fit=scale-down,dpr=1,width=154/uploads/local_template_image/image/406/persistent-resource/vienna-resume-templates.jpg'
+                },
+                {
+                    label: 'Lndn',
+                    img: 'https://s3.resume.io/cdn-cgi/image/format=auto,fit=scale-down,dpr=1,width=154/uploads/local_template_image/image/481/persistent-resource/london-resume-templates.jpg'
+                },
             ]
         },
 
