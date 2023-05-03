2d86a5436a0b37f4473bf94ca27f443250c319b8
83mbyte <registered.mail.mb@gmail.com>
2023-05-03 17:55:30 +0300

    PdfBtn init commit

---
+++ src/components/PdfBtn/PdfBtn.js
@@ -0,0 +1,54 @@
+import { Box, IconButton } from '@chakra-ui/react';
+import React from 'react';
+import { BsFiletypePdf } from "react-icons/bs";
+import styles from './PdfBtn.module.css';
+import { motion } from 'framer-motion';
+
+const animationType = {
+    newspaper: {
+        hidden: {
+            transform: "scale(0) rotate(720deg)",
+            opacity: 0,
+            transition: {
+                delay: 0.3,
+            },
+        },
+        visible: {
+            transform: " scale(1) rotate(0deg)",
+            opacity: 1,
+            transition: {
+                duration: 0.5,
+            },
+        },
+        exit: {
+            transform: "scale(0) rotate(-720deg)",
+            opacity: 0,
+            transition: {
+                duration: 0.3,
+            },
+        },
+    },
+}
+
+const PdfBtn = ({ onClickAction }) => {
+    return (
+        <Box className={styles.pdfBtnContainer} onClick={onClickAction} as={motion.div}
+            variants={animationType.newspaper}
+            initial="hidden"
+            animate="visible"
+            exit="exit"
+        >
+
+            <IconButton
+                variant='solid'
+                colorScheme='teal'
+                aria-label='Export to PDF'
+                fontSize='30px'
+                size={'md'}
+                icon={<BsFiletypePdf />}
+            />
+        </Box>
+    );
+};
+
+export default PdfBtn;
