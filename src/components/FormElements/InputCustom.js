c8ec90db78ff5e7a4bd619ed6e26436d809c55fe
83mbyte <registered.mail.mb@gmail.com>
2023-05-08 19:32:25 +0300

    validateNumbers added to handle numeric inputs

--- src/components/FormElements/InputCustom.js
+++ src/components/FormElements/InputCustom.js
@@ -6,7 +6,6 @@ import {
 
 const InputCustom = ({ name, onChangeCallback, inputValue = '', labelText = '', required = false, disabled = false, type = 'text', errorMessage = `Can't be empty.` }) => {
 
-    // const isError = inputValue === '';
     const inputRef = useRef(null);
     const ARRAYSIZE = ['xs', 'md', 'md'];
 
@@ -18,35 +17,64 @@ const InputCustom = ({ name, onChangeCallback, inputValue = '', labelText = '', 
             setIsError(false)
         }
     }
+    const validateNumbers = (name, value) => {
+        const regex = /[0-9]*/g;
+        let tmpstr = value.match(regex);
+        onChangeCallback(name, tmpstr.join(''))
+    }
 
-
-    return (
-        <Box p={2} w='full'>
-            <FormControl variant={'floating'} isRequired={required} isInvalid={isError && required} >
-                <Input
-                    size={ARRAYSIZE}
-                    value={inputValue}
-                    ref={inputRef}
-                    onChange={() => {
-                        checkRequired(inputRef.current.value)
-                        onChangeCallback(name, inputRef.current.value)
-                    }
-                    }
-                    onBlur={() => checkRequired(inputRef.current.value)}
-                    bg="white"
-                    placeholder=" "
-                    _focusVisible={{ 'boxShadow': 'none' }}
-                    disabled={disabled}
-                    type={type}
-                    name={name}
-                />
-                <FormLabel fontSize={ARRAYSIZE}>{labelText}</FormLabel>
-                {isError && (
-                    <FormErrorMessage>{errorMessage}</FormErrorMessage>
-                )}
-            </FormControl>
-        </Box>
-    );
+    if (type === 'number') {
+        return (
+            <Box p={2} w='full'>
+                <FormControl variant={'floating'} isRequired={required} isInvalid={isError && required} >
+                    <Input
+                        size={ARRAYSIZE}
+                        value={inputValue}
+                        ref={inputRef}
+                        onChange={() => validateNumbers(name, inputRef.current.value)}
+                        bg="white"
+                        placeholder=" "
+                        _focusVisible={{ 'boxShadow': 'none' }}
+                        disabled={disabled}
+                        name={name}
+                    />
+                    <FormLabel fontSize={ARRAYSIZE}>{labelText}</FormLabel>
+                    {isError && (
+                        <FormErrorMessage>{errorMessage}</FormErrorMessage>
+                    )}
+                </FormControl>
+            </Box>
+        );
+    }
+    else {
+        return (
+            <Box p={2} w='full'>
+                <FormControl variant={'floating'} isRequired={required} isInvalid={isError && required} >
+                    <Input
+                        size={ARRAYSIZE}
+                        value={inputValue}
+                        ref={inputRef}
+                        onChange={() => {
+                            checkRequired(inputRef.current.value)
+                            onChangeCallback(name, inputRef.current.value)
+                        }
+                        }
+                        onBlur={() => checkRequired(inputRef.current.value)}
+                        bg="white"
+                        placeholder=" "
+                        _focusVisible={{ 'boxShadow': 'none' }}
+                        disabled={disabled}
+                        type={type}
+                        name={name}
+                    />
+                    <FormLabel fontSize={ARRAYSIZE}>{labelText}</FormLabel>
+                    {isError && (
+                        <FormErrorMessage>{errorMessage}</FormErrorMessage>
+                    )}
+                </FormControl>
+            </Box>
+        );
+    }
 };
 
 export default InputCustom;
