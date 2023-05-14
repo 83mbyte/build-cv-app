eb8ba48f2240c94c63133ec082f9fa1f73db3025
83mbyte <registered.mail.mb@gmail.com>
2023-05-14 14:33:24 +0300

    circle,square types added

--- src/components/StarRating/StarRating.js
+++ src/components/StarRating/StarRating.js
@@ -4,37 +4,36 @@ import DOMPurify from 'dompurify';
 
 import styles from './StarRating.module.css';
 
-const StarRating = ({ level }) => {
+const StarRating = ({ level, type, customSize = null }) => {
     let point;
+    let itemEmpty;
+    let itemFilled;
 
-    switch (level) {
-        case 1:
-            point = '&#9733;'.repeat(1);
-            point += '&#9734;'.repeat(4)
-            break;
-        case 2:
-            point = '&#9733;'.repeat(2);
-            point += '&#9734;'.repeat(3)
-            break;
-        case 3:
-            point = '&#9733;'.repeat(3);
-            point += '&#9734;'.repeat(2)
-            break;
-        case 4:
-            point = '&#9733;&nbsp;'.repeat(4);
-            point += '&#9734;'.repeat(1)
-            break;
-        case 5:
-            point = '&#9733;'.repeat(5);
-            break;
-        default:
-            point = '&#9733;'.repeat(3);
-            point += '&#9734;'.repeat(2)
-            break;
+    if (type === 'square') {
+        itemEmpty = '&#9723;';
+        itemFilled = '&#9724;';
+
     }
+    else if (type === 'circle') {
+        itemEmpty = '&#9675;';
+        itemFilled = '&#9673;';
+    }
+    else {
+        itemEmpty = '&#9733;';
+        itemFilled = '&#9734;';
+    }
+
+    point = itemFilled.repeat(level) + itemEmpty.repeat(5 - level)
+
     return (
         // <span style={{ fontSize: '5px', letterSpacing: '7px' }} dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(point) }}></span>
-        <span className={styles.star} dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(point) }}></span>
+        <>
+            {
+                !customSize
+                    ? <span className={styles.item} dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(point) }}></span>
+                    : <span className={styles.item} style={{ fontSize: `${customSize}` }} dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(point) }}></span>
+            }
+        </>
 
     );
 };
