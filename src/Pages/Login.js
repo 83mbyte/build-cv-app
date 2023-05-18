cd7d53dc74f6399f5975d70f8ddb453fe59b9491
83mbyte <registered.mail.mb@gmail.com>
2023-05-18 14:36:36 +0300

    Option to sign in with Google addded.

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -1,21 +1,17 @@
-import { VStack, Heading, Box, Button, HStack, Text } from '@chakra-ui/react';
+import { VStack, Heading, Box, Button, HStack, Text, Divider } from '@chakra-ui/react';
 import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Link as RouterLink, useNavigate, } from 'react-router-dom';
 
-
+import { FcGoogle } from 'react-icons/fc';
 import WhiteboxCenter from '../components/Wrappers/WhiteboxCenter';
 import AlertScaleFade from '../components/Alert/AlertScaleFade';
 import InputCustom from '../components/FormElements/InputCustom';
-import { clearAuthError, logInThunk } from '../redux/features/utility/utilitySlice';
+import { clearAuthError, signInGoogleThunk, logInThunk } from '../redux/features/utility/utilitySlice';
 
-const Login_beta = () => {
-    // Remove data (email,pass) from inputsValue
-    // Remove data (email,pass) from inputsValue
+const Login = () => {
     const [inputsValue, setInputsValue] = useState({ email: '', pass: '' });
     // const [inputsValue, setInputsValue] = useState({ email: process.env.REACT_APP_TEMP_EMAIL, pass: process.env.REACT_APP_TEMP_PASS });
-    // Remove data (email,pass) from inputsValue
-    // Remove data (email,pass) from inputsValue
 
     const sizeBreakPoints = ['sm', 'md'];
     const user = useSelector(state => state.utility.auth.data);
@@ -40,26 +36,35 @@ const Login_beta = () => {
             [name]: value
         })
     }
-    const handleClickLogin = () => {
+    const handleSignInBtnClick = () => {
         if (inputsValue.email !== '' && inputsValue.pass !== '') {
             dispatch(logInThunk({ email: inputsValue.email, pass: inputsValue.pass }))
         } else {
             console.log('data not provided')
         }
     }
+
+    const googleSignIn = () => {
+        dispatch(signInGoogleThunk(true))
+    }
+
     useEffect(() => {
         if (status === 'ready' && error === '' && user && user.userId) {
             navigate("/dashboard")
         }
     }, [status, error, navigate, user]);
 
-    return (
+    useEffect(() => {
+        dispatch(signInGoogleThunk(false))
+    }, []);
 
+
+    return (
 
         <WhiteboxCenter>
             <VStack spacing={[3, 5]} w='full' align={['flex-start', 'center']}>
                 <VStack align={['flex-start', 'center']} w='full'>
-                    <Heading as='h2'>Login</Heading>
+                    <Heading as='h2'>Sign In</Heading>
                 </VStack>
                 <Box w='full'>
                     <InputCustom labelText='Email' required name={'email'} inputValue={inputsValue['email']} onChangeCallback={onChangeInput} />
@@ -75,18 +80,43 @@ const Login_beta = () => {
                         colorScheme={'teal'}
                         size={sizeBreakPoints} rounded={sizeBreakPoints}
                         isLoading={status === 'loading'}
-                        onClick={handleClickLogin}
+                        onClick={handleSignInBtnClick}
                         isDisabled={error !== '' || inputsValue.pass.length < 6}
-                    >Login</Button>
+                    >Sign In</Button>
                 </Box>
+
                 <HStack w='full' justify={'center'} spacing={'1'} fontSize={'xs'} alignItems={'baseline'}>
-                    <Text>Not registered? Click to </Text>
-                    <Button variant={'link'} colorScheme={'teal'} as={RouterLink} to="/signup" fontSize={'xs'} mx={0} px={0} onClick={clearError} >SignUp</Button>
+                    <Text>Not registered yet? Click to </Text>
+                    <Button variant={'link'} colorScheme={'teal'} as={RouterLink} to="/signup" fontSize={'xs'} mx={0} px={0} onClick={clearError} >Sign Up</Button>
                 </HStack>
+                <Box p={2} w='full'>
+                    <VStack spacing={[3, 5]} >
+                        <HStack w={'full'} justifyContent={'space-between'}>
+                            <Divider w={'full'} />
+                            <Text backgroundColor={''} textAlign={'center'} px={1} w={10} fontSize={'xs'}>else</Text>
+                            <Divider w={'full'} />
+                        </HStack>
+                        <Box w='full' bg=''>
+
+                            <Button
+                                onClick={googleSignIn}
+                                w={'full'}
+                                size={sizeBreakPoints} rounded={sizeBreakPoints}
+                                variant={'outline'}
+                                colorScheme='teal'
+                                leftIcon={<FcGoogle />}
+                                _hover={{ backgroundColor: 'white' }}>
+                                {/* Sign in with Google */}
+                                <Text>Sign In with Google</Text>
+                            </Button>
+
+                        </Box>
+                    </VStack>
+                </Box>
             </VStack>
         </WhiteboxCenter>
 
     );
 };
 
-export default Login_beta;
+export default Login;
