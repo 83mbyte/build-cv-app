53f4d0dedec0aec445d602904ed2adadb87385c3
83mbyte <registered.mail.mb@gmail.com>
2023-05-23 13:57:18 +0300

    AlternativeSignInBtn added

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -8,6 +8,7 @@ import WhiteboxCenter from '../components/Wrappers/WhiteboxCenter';
 import AlertScaleFade from '../components/Alert/AlertScaleFade';
 import InputCustom from '../components/FormElements/InputCustom';
 import { clearAuthError, signInGoogleThunk, logInThunk } from '../redux/features/utility/utilitySlice';
+import AlternativeSignInBtn from '../components/AlternativeSignInBtn/AlternativeSignInBtn';
 
 const Login = () => {
     const [inputsValue, setInputsValue] = useState({ email: '', pass: '' });
@@ -55,8 +56,12 @@ const Login = () => {
     }, [status, error, navigate, user]);
 
     useEffect(() => {
-        dispatch(signInGoogleThunk(false))
-    }, []);
+
+        if (sessionStorage.getItem(`firebase:pendingRedirect:${process.env.REACT_APP_API_KEY}:[DEFAULT]`)) {
+
+            dispatch(signInGoogleThunk(false));
+        }
+    }, [dispatch]);
 
 
     return (
@@ -66,53 +71,32 @@ const Login = () => {
                 <VStack align={['flex-start', 'center']} w='full'>
                     <Heading as='h2'>Sign In</Heading>
                 </VStack>
-                <Box w='full'>
-                    <InputCustom labelText='Email' required name={'email'} inputValue={inputsValue['email']} onChangeCallback={onChangeInput} />
-                    <InputCustom labelText='Password' name={'pass'} type={'password'} inputValue={inputsValue['pass']} onChangeCallback={onChangeInput} errorMessage={'Must be at least 6 characters long.'} required />
-                </Box>
-                {
-                    (error !== '' || successMsg !== '')
-                    && <AlertScaleFade message={error !== '' ? error : successMsg} type={error !== '' ? 'error' : 'success'} />
-                }
-                <Box p={2} w='full'>
-                    <Button
-                        w='full'
-                        colorScheme={'teal'}
-                        size={sizeBreakPoints} rounded={sizeBreakPoints}
-                        isLoading={status === 'loading'}
-                        onClick={handleSignInBtnClick}
-                        isDisabled={error !== '' || inputsValue.pass.length < 6}
-                    >Sign In</Button>
-                </Box>
+
+                <LoginForm
+                    error={error}
+                    status={status}
+                    successMsg={successMsg}
+                    inputsValue={inputsValue}
+                    sizeBreakPoints={sizeBreakPoints}
+                    handleSignInBtnClick={handleSignInBtnClick}
+                    onChangeInput={onChangeInput}
+                />
 
                 <HStack w='full' justify={'center'} spacing={'1'} fontSize={'xs'} alignItems={'baseline'}>
                     <Text>Not registered yet? Click to </Text>
                     <Button variant={'link'} colorScheme={'teal'} as={RouterLink} to="/signup" fontSize={'xs'} mx={0} px={0} onClick={clearError} >Sign Up</Button>
                 </HStack>
-                <Box p={2} w='full'>
-                    <VStack spacing={[3, 5]} >
-                        <HStack w={'full'} justifyContent={'space-between'}>
-                            <Divider w={'full'} />
-                            <Text backgroundColor={''} textAlign={'center'} px={1} w={10} fontSize={'xs'}>else</Text>
-                            <Divider w={'full'} />
-                        </HStack>
-                        <Box w='full' bg=''>
 
-                            <Button
-                                onClick={googleSignIn}
-                                w={'full'}
-                                size={sizeBreakPoints} rounded={sizeBreakPoints}
-                                variant={'outline'}
-                                colorScheme='teal'
-                                leftIcon={<FcGoogle />}
-                                _hover={{ backgroundColor: 'white' }}>
-                                {/* Sign in with Google */}
-                                <Text>Sign In with Google</Text>
-                            </Button>
+                <AlternativeSignInForm>
 
-                        </Box>
-                    </VStack>
-                </Box>
+                    <AlternativeSignInBtn
+                        providerName={'Google'}
+                        sizeBreakPoints={sizeBreakPoints}
+                        icon={<FcGoogle />}
+                        onClickFn={googleSignIn}
+                    />
+                </AlternativeSignInForm>
+
             </VStack>
         </WhiteboxCenter>
 
@@ -120,3 +104,46 @@ const Login = () => {
 };
 
 export default Login;
+
+const LoginForm = ({ error, status, successMsg, inputsValue, sizeBreakPoints, handleSignInBtnClick, onChangeInput }) => {
+    return (
+        <>
+            <Box w='full'>
+                <InputCustom labelText='Email' required name={'email'} inputValue={inputsValue['email']} onChangeCallback={onChangeInput} />
+                <InputCustom labelText='Password' name={'pass'} type={'password'} inputValue={inputsValue['pass']} onChangeCallback={onChangeInput} errorMessage={'Must be at least 6 characters long.'} required />
+            </Box>
+            {
+                (error !== '' || successMsg !== '')
+                && <AlertScaleFade message={error !== '' ? error : successMsg} type={error !== '' ? 'error' : 'success'} />
+            }
+            <Box p={2} w='full'>
+                <Button
+                    w='full'
+                    colorScheme={'teal'}
+                    size={sizeBreakPoints} rounded={sizeBreakPoints}
+                    isLoading={status === 'loading'}
+                    onClick={handleSignInBtnClick}
+                    isDisabled={error !== '' || inputsValue.pass.length < 6}
+                >Sign In</Button>
+            </Box>
+        </>
+    )
+}
+
+
+const AlternativeSignInForm = ({ children }) => {
+    return (
+        <Box p={2} w='full'>
+            <VStack spacing={[3, 5]} >
+                <HStack w={'full'} justifyContent={'space-between'}>
+                    <Divider w={'full'} />
+                    <Text backgroundColor={''} textAlign={'center'} px={1} w={10} fontSize={'xs'}>else</Text>
+                    <Divider w={'full'} />
+                </HStack>
+                <Box w='full' bg=''>
+                    {children}
+                </Box>
+            </VStack >
+        </Box >
+    )
+}
