08cd213557731faaeb90f73149efd40ae4d9e1fa
83mbyte <registered.mail.mb@gmail.com>
2023-07-03 14:58:35 +0300

    BackIcon added

--- src/components/Icons/Icon.js
+++ src/components/Icons/Icon.js
@@ -3,7 +3,7 @@ import { Box } from "@chakra-ui/react";
 import { IconContext } from "react-icons";
 
 import { FaPencilAlt, FaEyeSlash } from "react-icons/fa";
-import { MdDelete, MdExpandMore, MdClose } from 'react-icons/md';
+import { MdDelete, MdExpandMore, MdClose, MdArrowBack } from 'react-icons/md';
 import styles from './IconStyles.module.css';
 
 
@@ -25,6 +25,17 @@ export const CloseIcon = () => {
         <IconContext.Provider value={{ className: `${styles.icon}` }} >
             <Box _hover={{ color: 'teal', cursor: 'pointer' }} color={'gray.400'} height={['18px', '24px']} w={['18px', '24px']} p={0}>
                 <MdClose />
+            </Box >
+        </IconContext.Provider >
+    )
+}
+
+export const BackIcon = () => {
+
+    return (
+        <IconContext.Provider value={{ className: `${styles.icon}` }} >
+            <Box _hover={{ color: 'teal', cursor: 'pointer' }} color={'gray.400'} height={['18px', '24px']} w={['18px', '24px']} p={0}>
+                <MdArrowBack />
             </Box >
         </IconContext.Provider >
     )
