c00dad6ca526dafdf8451be93367394ce19d8eab
83mbyte <registered.mail.mb@gmail.com>
2023-07-03 15:06:06 +0300

    DrawerCoverLetterContainer, CoverLetterSection, CreateCover init commit

---
+++ src/components/Drawer/CoverLetterSection/CoverLetterSection.js
@@ -0,0 +1,40 @@
+import {
+    Button, Heading, Wrap, WrapItem
+} from '@chakra-ui/react';
+
+import React, { useRef, useState } from 'react';
+
+import { IoDocumentTextOutline, IoBulbOutline } from "react-icons/io5";
+import DrawerCoverLetterContainer from './DrawerCoverLetterContainer';
+
+const CoverLetterSection = () => {
+
+    const createRef = useRef(null);
+    const generateRef = useRef(null);
+
+    const [isOpen, setIsOpen] = useState({ visible: false, type: null });
+
+    const onClickBtnHandler = (type) => {
+        setIsOpen({
+            visible: true,
+            type: type
+        })
+    }
+
+    return (
+
+        <>
+            <Heading as={'h5'} size={'xs'} mb={2}>Create a cover letter</Heading>
+            <Wrap px={2}>
+                <WrapItem>
+                    <Button ref={createRef} size={'sm'} colorScheme={'teal'} variant={'outline'} leftIcon={<IoDocumentTextOutline />} onClick={() => onClickBtnHandler('Create')}>Create/Edit</Button>
+                </WrapItem>
+                <WrapItem><Button ref={generateRef} size={'sm'} isDisabled colorScheme={'teal'} variant={'outline'} leftIcon={<IoBulbOutline />} onClick={() => onClickBtnHandler('Generate')}>Generate</Button></WrapItem>
+
+            </Wrap>
+            <DrawerCoverLetterContainer isOpen={isOpen} setIsOpen={setIsOpen} />
+        </>
+    )
+};
+
+export default CoverLetterSection;
---
+++ src/components/Drawer/CoverLetterSection/DrawerCoverLetterContainer.js
@@ -0,0 +1,85 @@
+import React, { Suspense, lazy, useEffect } from 'react';
+import {
+    Drawer,
+    DrawerBody,
+    DrawerHeader,
+    DrawerOverlay,
+    DrawerContent,
+    HStack, Box, Center, Flex, Container
+} from '@chakra-ui/react';
+import { BackIcon } from '../../Icons/Icon';
+import { useDispatch, useSelector } from 'react-redux';
+import { getCoverLetter, inputCoverUpdate } from '../../../redux/features/coverLetter/coverLetterSlice';
+import { setIsModifiedContent } from '../../../redux/features/utility/utilitySlice';
+
+const CreateCover = lazy(() => import('./CreateCover'));
+
+const DrawerCoverLetterContainer = ({ isOpen, setIsOpen }) => {
+    const loggedUser = useSelector(state => state.utility.auth.data);
+    const dispatch = useDispatch();
+
+    const data = useSelector(state => state.cover.data);
+    const status = useSelector(state => state.cover.status);
+    const error = useSelector(state => state.cover.error);
+
+    const onChangeHandler = (data) => {
+        dispatch(setIsModifiedContent({ status: true, section: 'cover' }));
+        dispatch(inputCoverUpdate({ value: data }));
+    }
+
+    useEffect(() => {
+        if (status === 'idle') {
+            dispatch(getCoverLetter(loggedUser))
+        }
+    }, [status, dispatch, loggedUser])
+
+    return (
+        <Drawer onClose={() => setIsOpen({ visible: false, type: null })} isOpen={isOpen.visible} size={'full'} placement='left'>
+            <DrawerOverlay />
+            <DrawerContent>
+                <DrawerHeader borderBottomWidth='1px'>
+                    <HStack>
+                        <Box bg='' onClick={() => setIsOpen({ visible: false, type: null })} >
+                            <BackIcon />
+                        </Box>
+                        <Box w='full' >
+                            <Center>Cover Letter</Center>
+                        </Box>
+                    </HStack>
+                </DrawerHeader>
+                <DrawerBody className='mainBg' >
+                    <Flex bg='transparent' flexDirection={'column'} alignItems={'center'} justifyContent={'center'} w='full' h={'90%'}>
+                        <Container
+                            as='div'
+                            bg={'white'}
+                            border={'1px'}
+                            borderColor={'gray.200'}
+                            borderRadius={10}
+                            maxW={'2xl'}
+                            p={0} my={0}
+                            mx={'auto'}
+                        >
+                            {
+                                isOpen.type === 'Create'
+                                    ? <Suspense>
+                                        <CreateCover state={{ data, status, error }} onChangeHandler={onChangeHandler} />
+                                    </Suspense>
+                                    : <GenerateCover />
+                            }
+                        </Container>
+                    </Flex>
+                </DrawerBody>
+            </DrawerContent>
+        </Drawer>
+    );
+};
+
+export default DrawerCoverLetterContainer;
+
+
+
+const GenerateCover = () => {
+    return (
+        <Box>Generate</Box>
+    )
+}
