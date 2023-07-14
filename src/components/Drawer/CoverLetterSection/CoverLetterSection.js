cca1d28a836747d8cb3104001f72b4044a1b77d7
83mbyte <registered.mail.mb@gmail.com>
2023-07-14 14:01:25 +0300

    added ToolTip for the buttons

--- src/components/Drawer/CoverLetterSection/CoverLetterSection.js
+++ src/components/Drawer/CoverLetterSection/CoverLetterSection.js
@@ -6,6 +6,7 @@ import React, { useRef, useState } from 'react';
 
 import { IoDocumentTextOutline, IoBulbOutline } from "react-icons/io5";
 import DrawerCoverLetterContainer from './DrawerCoverLetterContainer';
+import ToolTip from '../../Tooltip/ToolTip';
 
 const CoverLetterSection = () => {
 
@@ -27,9 +28,15 @@ const CoverLetterSection = () => {
             <Heading as={'h5'} size={'xs'} mb={2}>Create a cover letter</Heading>
             <Wrap px={2}>
                 <WrapItem>
-                    <Button ref={createRef} size={'sm'} colorScheme={'teal'} variant={'outline'} leftIcon={<IoDocumentTextOutline />} onClick={() => onClickBtnHandler('Create')}>New / Edit</Button>
+                    <ToolTip label='create new or edit your cover letter'>
+                        <Button ref={createRef} size={'sm'} colorScheme={'teal'} variant={'outline'} leftIcon={<IoDocumentTextOutline />} onClick={() => onClickBtnHandler('Create')}>New / Edit</Button>
+                    </ToolTip>
                 </WrapItem>
-                <WrapItem><Button ref={generateRef} size={'sm'} colorScheme={'teal'} variant={'outline'} leftIcon={<IoBulbOutline />} onClick={() => onClickBtnHandler('Generate')}>Generate</Button></WrapItem>
+                <WrapItem>
+                    <ToolTip label='generate cover letter automatically'>
+                        <Button ref={generateRef} size={'sm'} colorScheme={'teal'} variant={'outline'} leftIcon={<IoBulbOutline />} onClick={() => onClickBtnHandler('Generate')}>Generate</Button>
+                    </ToolTip>
+                </WrapItem>
 
             </Wrap>
             <DrawerCoverLetterContainer isOpen={isOpen} setIsOpen={setIsOpen} />
