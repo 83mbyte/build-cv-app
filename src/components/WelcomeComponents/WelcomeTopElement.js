8ac6763c7ea9d7a8b56e4fe8bc974ee6ddee720a
83mbyte <registered.mail.mb@gmail.com>
2023-08-02 14:57:49 +0300

    added WelcomeWrapper component as a content container

--- src/components/WelcomeComponents/WelcomeTopElement.js
+++ src/components/WelcomeComponents/WelcomeTopElement.js
@@ -1,10 +1,11 @@
 
 import React from 'react';
-import { Box, Heading, Highlight, Text, VStack, Button, ButtonGroup } from '@chakra-ui/react';
+import { Box, Heading, Highlight, Text, Button, ButtonGroup } from '@chakra-ui/react';
 import { motion } from "framer-motion";
 
 import { useNavigate } from 'react-router-dom';
 import WelcomeBlockImage from './WelcomeBlockImage';
+import WelcomeWrapper from '../Wrappers/WelcomeWrapper';
 const WelcomeTopElement = () => {
     const navigate = useNavigate();
     const apiKey = process.env.REACT_APP_API_KEY;
@@ -33,44 +34,27 @@ const WelcomeTopElement = () => {
         </ButtonGroup >
     }
     return (
-        <Box
-            bg='white'
-            w='full'
-            py={'10'}
-            pb={'0'}
-            px={0}
-            mx={2}
-            borderTopWidth={'1px'}
-            borderColor={'gray.300'}
-            borderLeft={'none'}
-            borderRight={'none'}
-            justifyContent={'center'}
-            display={'flex'}
-            flexDirection={'column'}
-        >
-            <VStack bg={''} w={['full', 'xl', '2xl']} mx={'auto'} spacing={'3'} >
-                <Text textAlign={'center'} fontSize={['sm', 'lg']}>AI-powered Resume Builder</Text>
-                <Box as={motion.div}
-                    initial={{ y: -50, opacity: 0 }}
-                    whileInView={{ y: 0, opacity: 1, transition: { delay: 0.3, duration: 0.8 } }}
-                >
-                    <Heading as='h1' size={['xl', '2xl']} textAlign={'center'} >
-                        Effortlessly make a job-worthy <Highlight query={'resume'} styles={{ px: '0.5', pt: '0', pb: '0.5', bg: 'rgba(250,250,137,0.8)', }}>resume</Highlight> that gets you hired faster.
-                    </Heading>
-                </Box>
 
-                <Box bg='' w={['xs', 'md']} >
-                    <Text color={'gray.500'} textAlign={'center'} fontSize={['sm', 'md']} bg={'rgba(250,250,250,0.4)'}>Use field-tested templates that follow the exact so-called "resume rules" hirers look for.
-                        <Highlight query={'Easy to use'} styles={{ px: '0.5', py: '0.5', bg: 'rgba(250,250,137,0.8)', color: 'gray.500' }}>Easy to use - instant result.</Highlight></Text>
-                </Box>
-                <Box justifyContent={'center'} display={'flex'} py={3}>
-                    {AccessAreaButtons}
-                </Box>
-            </VStack>
+        <WelcomeWrapper firstPosition={true}>
+            <Text textAlign={'center'} fontSize={['sm', 'lg']}>AI-powered Resume Builder</Text>
+            <Box as={motion.div}
+                initial={{ y: -50, opacity: 0 }}
+                whileInView={{ y: 0, opacity: 1, transition: { delay: 0.3, duration: 0.8 } }}
+            >
+                <Heading as='h1' size={['xl', '2xl']} textAlign={'center'} >
+                    Effortlessly make a job-worthy <Highlight query={'resume'} styles={{ px: '0.5', pt: '0', pb: '0.5', bg: 'rgba(250,250,137,0.8)', }}>resume</Highlight> that gets you hired faster.
+                </Heading>
+            </Box>
 
+            <Box bg='' w={['xs', 'md']} >
+                <Text color={'gray.500'} textAlign={'center'} fontSize={['sm', 'md']} bg={'rgba(250,250,250,0.4)'}>Use field-tested templates that follow the exact so-called "resume rules" hirers look for.
+                    <Highlight query={'Easy to use'} styles={{ px: '0.5', py: '0.5', bg: 'rgba(250,250,137,0.8)', color: 'gray.500' }}>Easy to use - instant result.</Highlight></Text>
+            </Box>
+            <Box justifyContent={'center'} display={'flex'} py={3}>
+                {AccessAreaButtons}
+            </Box>
             <WelcomeBlockImage imgName={'welcome1.jpg'} />
-        </Box >
-
+        </WelcomeWrapper>
     );
 };
 
