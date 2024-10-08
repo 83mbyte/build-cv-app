1fde553918e336e1beb0c6c1b954ff3b56b53a58
83mbyte <registered.mail.mb@gmail.com>
2024-10-08 14:29:54 +0300

    TextEditor updated to use react-simple-wysiwyg editor

--- src/components/FormItems/TextEditor/TextEditor.js
+++ src/components/FormItems/TextEditor/TextEditor.js
@@ -1,74 +1,75 @@
+import { useState } from 'react';
 import { Box, Text } from '@chakra-ui/react';
 
-import { useState, useEffect } from 'react';
-
-import { EditorState } from 'draft-js';
-import { Editor } from 'react-draft-wysiwyg';
-import { convertFromHTML, convertToHTML } from 'draft-convert';
-import 'react-draft-wysiwyg/dist/react-draft-wysiwyg.css';
+import {
+    BtnBold,
+    BtnItalic,
+    BtnUnderline,
+    BtnBulletList,
+    BtnUndo, BtnRedo,
+    BtnNumberedList,
+    Editor,
+    EditorProvider,
+    Toolbar
+} from 'react-simple-wysiwyg';
 import './TextEditor.css';
 
 import sanitizeString from '@/lib/sanitizeString';
 
-const TextEditor = ({ state, onChangeCallback, heightSize = null }) => {
-    const [editorState, setEditorState] = useState(
-        () => EditorState.createWithContent(convertFromHTML(state.value))
-    );
+const TextEditor = ({ state, onChangeCallback }) => {
 
-    const [convertedContent, setConvertedContent] = useState(null);
-
-    useEffect(() => {
-        let html = convertToHTML(editorState.getCurrentContent());
-        setConvertedContent(html);
-    }, [editorState]);
-
-    useEffect(() => {
-        if (state.value) {
-            setEditorState(EditorState.createWithContent(convertFromHTML(state.value)))
+    const [html, setHtml] = useState(() => {
+        if (state?.value) {
+            return state.value
+        } else {
+            return ''
         }
-    }, [state.value])
+    });
+
+    function onChange(e) {
+        setHtml(e.target.value);
+    }
 
     const applyChanges = () => {
-        if ((convertedContent && convertedContent !== ' ' && convertedContent !== '')) {
-            if (convertedContent === '<p></p>' || convertedContent === '<p> </p>') {
-                onChangeCallback('')
-            } else {
-                onChangeCallback(sanitizeString(convertedContent));
-            }
+        if (html && html !== '') {
+            let cleanString = sanitizeString(html);
+            onChangeCallback(cleanString);
         }
     }
 
     return (
-        <Box my={state.label === '' ? '3' : '0'} w={'100%'} px={[1, 0]} minW={'200px'}>
+        <Box my={state?.label === '' ? '3' : '0'} w={'100%'} px={[1, 0]} minW={'200px'}>
 
-            {state.label && <Text color={'gray.500'} px={2} mx={3} mb={'-2px'} fontSize={'xs'} fontWeight={'semibold'}>{state.label}</Text>}
-            <Editor
-                editorState={editorState}
-                onEditorStateChange={setEditorState}
-                wrapperClassName="wrapper-class"
-                toolbarClassName="toolbar-class"
-                editorClassName={heightSize ? 'editor-class coverLetter' : 'editor-class'}
-                ariaLabel='textEditorLabel'
-                toolbar={{
-                    options: ['inline', 'list', 'history'],
-                    separator: '|',
-                    inline: {
-                        inDropdown: false,
-                        options: ['bold', 'italic', 'underline', 'strikethrough']
-                    },
-                    list: {
-                        inDropdown: false,
-                        options: ['unordered', 'ordered',]
-                    },
+            {state?.label && <Text color={'gray.500'} px={2} mx={3} mb={'-2px'} fontSize={'xs'} fontWeight={'semibold'}>{state.label}</Text>}
+            <EditorProvider>
+                <Editor value={html} onChange={onChange} onBlur={applyChanges} >
+                    <Toolbar  >
+                        <BtnUndo />
+                        <BtnRedo />
 
-                    fontSize: {
-                        options: [10, 11, 12, 14, 16, 18, 24],
-                    },
-                }}
-                onBlur={applyChanges}
-            />
-        </Box >
+                        <Toolbar_Separator />
+                        <BtnBold />
+                        <BtnItalic />
+                        <BtnUnderline />
+
+                        <Toolbar_Separator />
+                        <BtnNumberedList style={{ marginLeft: '10px' }} />
+                        <BtnBulletList />
+                    </Toolbar>
+                </Editor>
+            </EditorProvider>
+        </Box>
     );
 };
 
 export default TextEditor;
+
+const Toolbar_Separator = () => {
+    return (
+        <div style={{
+            backgroundColor: 'rgb(226, 232, 240)',
+            width: '1px',
+            height: '100%'
+        }}>&nbsp;</div>
+    )
+}
