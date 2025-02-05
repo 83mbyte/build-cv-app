461df6ceda033347d079e820cebf4410f11c0097
83mbyte <registered.mail.mb@gmail.com>
2025-02-05 11:46:24 +0200

    scripts to use with CustomLink/Text/Heading components

---
+++ src/lib/commonScripts.js
@@ -0,0 +1,22 @@
+import DOMPurify from "isomorphic-dompurify";
+
+export const preventEnterButton = (e) => {
+    if (e.code == 'Enter') {
+        e.preventDefault();
+    }
+}
+
+export const sanitizeInput = (dirtyString) => {
+    let cleanString = DOMPurify.sanitize(dirtyString, {
+        ALLOWED_TAGS: ['b', 'i', 'a', '<br>'],
+        ALLOWED_ATTR: ['style', 'href']
+    });
+    return cleanString;
+}
+
+export const formatText = (stringToFormat) => {
+    let newLineRegex = /\n/g;
+    let httpsRegex = /(http[s]?:\/\/)+/gim;
+    let formatted = stringToFormat.trim().replaceAll(newLineRegex, '</br>').replaceAll(httpsRegex, '');
+    return formatted
+}
