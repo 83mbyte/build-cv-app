b96fc5bc32482ef0e4af83f97962f184ebdef55d
83mbyte <registered.mail.mb@gmail.com>
2025-02-10 16:29:06 +0200

    switches to show/hide resume blocks added

--- src/components/editor_page/editorHeader/HeaderLayoutMenu.js
+++ src/components/editor_page/editorHeader/HeaderLayoutMenu.js
@@ -1,13 +1,17 @@
 'use client'
 import { PopoverArrow, PopoverBody, PopoverContent, PopoverRoot } from "@/components/ui/popover";
-
-import { Box, Button, HStack, Text, PopoverTrigger, Icon, } from "@chakra-ui/react";
+import { Switch } from "@/components/ui/switch"
+import { Box, Button, HStack, Text, PopoverTrigger, Icon, VStack, } from "@chakra-ui/react";
 
 import { useDispatch, useSelector } from "react-redux";
 import { setIsHeaderMenuOpen, setLayout, setShowOverlay } from "@/redux/settings/editorSettingsSlice";
+import { setResumeSummaryIsVisible } from "@/redux/resume/summaryBlockSlice";
+import { setResumeEducationIsVisible } from "@/redux/resume/educationBlockSlice";
+import { setResumeExperienceIsVisible } from "@/redux/resume/experienceBlockSlice";
+import { setResumeSkillsIsVisible } from "@/redux/resume/skillsBlockSlice";
+import { setResumeLanguagesIsVisible } from "@/redux/resume/languagesBlockSlice";
 
 import { LuChevronDown, LuTableCellsSplit } from "react-icons/lu";
-
 
 const layouts = ['1', '2', '3']
 const HeaderLayoutMenu = () => {
@@ -15,6 +19,13 @@ const HeaderLayoutMenu = () => {
     const themeColor = useSelector(state => state.editorSettings.themeColor);
     const layout = useSelector(state => state.editorSettings.layout);
     const isMenuOpen = useSelector(state => state.editorSettings.isHeaderMenuOpen.layoutMenu);
+
+    // switch buttons status:
+    const isSummaryChecked = useSelector(state => state.resumeSummary.isVisible);
+    const isEducationChecked = useSelector(state => state.resumeEducation.isVisible);
+    const isSkillsChecked = useSelector(state => state.resumeSkills.isVisible);
+    const isLanguagesChecked = useSelector(state => state.resumeLanguages.isVisible);
+    const isExperienceChecked = useSelector(state => state.resumeExperience.isVisible);
     const dispatch = useDispatch();
 
     const changeLayout = (layoutIndex) => {
@@ -30,7 +41,7 @@ const HeaderLayoutMenu = () => {
         >
             <PopoverTrigger asChild>
                 <Button
-                    variant={'link'} colorPalette={'white'} size={'xs'}
+                    variant={'link'} colorPalette={'white'} size={['2xs', 'xs']}
                     _hover={{ opacity: 0.5 }}
                 >
                     <HStack gap={['1', '2']} >
@@ -44,31 +55,46 @@ const HeaderLayoutMenu = () => {
             <PopoverContent width='2xs'>
                 <PopoverArrow />
                 <PopoverBody>
-                    <Text fontSize={'xs'}>Variant:</Text>
-                    <Box borderWidth={1} borderColor={`${themeColor}.300`} marginY={'1'} borderRadius={'sm'} overflow={'hidden'} >
-                        <HStack _hover={{ cursor: 'pointer' }} gap={0} w='full'>
-                            {
-                                layouts.map((item, index) => {
-                                    return (
-                                        <Box flex={1}
-                                            key={index}
-                                            transition={'background-color 0.7s ease-in-out'}
-                                            backgroundColor={layout == index ? `${themeColor}.300` : 'transparent'}
+                    <VStack width='full' bg='' gap={4}>
+                        <Box w='full' >
+                            <Text fontSize={'xs'}>Variant:</Text>
+                            <Box borderWidth={1} borderColor={`${themeColor}.300`} marginY={'1'} borderRadius={'sm'} overflow={'hidden'} >
+                                <HStack _hover={{ cursor: 'pointer' }} gap={0} w='full'>
+                                    {
+                                        layouts.map((item, index) => {
+                                            return (
+                                                <Box flex={1}
+                                                    key={index}
+                                                    transition={'background-color 0.7s ease-in-out'}
+                                                    backgroundColor={layout == index ? `${themeColor}.300` : 'transparent'}
 
-                                            onClick={() => changeLayout(index)}
-                                        >
-                                            <Text textAlign={'center'} fontSize={['xs', 'sm']} fontWeight={'bold'}
-                                                color={layout == index ? 'white' : `${themeColor}.300`}
-                                                _hover={{ opacity: layout != index ? 0.5 : 1 }}
-                                                transition={'opacity 0.5s ease'}
+                                                    onClick={() => changeLayout(index)}
+                                                >
+                                                    <Text textAlign={'center'} fontSize={['xs', 'sm']} fontWeight={'bold'}
+                                                        color={layout == index ? 'white' : `${themeColor}.300`}
+                                                        _hover={{ opacity: layout != index ? 0.5 : 1 }}
+                                                        transition={'opacity 0.5s ease'}
 
-                                            >{item}</Text>
-                                        </Box>
-                                    )
-                                })
-                            }
-                        </HStack>
-                    </Box>
+                                                    >{item}</Text>
+                                                </Box>
+                                            )
+                                        })
+                                    }
+                                </HStack>
+                            </Box>
+                        </Box>
+                        {/* show blocks */}
+                        <Box w='full'>
+                            <Text fontSize={'xs'}>Show:</Text>
+                            <VStack w='full' alignItems={'flex-start'} gap={3} px={1}>
+                                <SwitchBlock label={'Summary'} isChecked={isSummaryChecked} actionToDispatch={setResumeSummaryIsVisible} />
+                                <SwitchBlock label={'Education'} isChecked={isEducationChecked} actionToDispatch={setResumeEducationIsVisible} />
+                                <SwitchBlock label={'Experience'} isChecked={isExperienceChecked} actionToDispatch={setResumeExperienceIsVisible} />
+                                <SwitchBlock label={'Skills'} isChecked={isSkillsChecked} actionToDispatch={setResumeSkillsIsVisible} />
+                                <SwitchBlock label={'Languages'} isChecked={isLanguagesChecked} actionToDispatch={setResumeLanguagesIsVisible} />
+                            </VStack>
+                        </Box>
+                    </VStack>
                 </PopoverBody>
             </PopoverContent>
         </PopoverRoot>
@@ -76,3 +102,21 @@ const HeaderLayoutMenu = () => {
 };
 
 export default HeaderLayoutMenu;
+
+const SwitchBlock = ({ label, isChecked, actionToDispatch }) => {
+    const themeColor = useSelector(state => state.editorSettings.themeColor);
+    const dispatch = useDispatch();
+    return (
+        <HStack bg='' justifyContent={'space-between'} w='full' alignItems={'center'}>
+
+            <Text fontSize={'xs'}>{label}</Text>
+            <Switch checked={isChecked}
+                size={'sm'}
+                colorPalette={themeColor}
+                onCheckedChange={(e) => dispatch(actionToDispatch({ show: e.checked }))}
+            />
+
+        </HStack>
+    )
+}
+
