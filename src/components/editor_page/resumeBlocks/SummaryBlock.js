bcca9da112276ad1a4b370524fcfd6f180356981
83mbyte <registered.mail.mb@gmail.com>
2025-02-28 11:56:32 +0200

    updated SummaryBlock to use inline styles while create PDF

--- src/components/editor_page/resumeBlocks/SummaryBlock.js
+++ src/components/editor_page/resumeBlocks/SummaryBlock.js
@@ -12,32 +12,83 @@ import CustomText from '../dataFields/CustomText';
 import BlockControlContainer from '../blockControl/BlockControlContainer';
 
 const SummaryBlock = ({ editableFields }) => {
+
     const blockName = 'resumeSummary';
-    const fontSize = useSelector(state => state.fontSettings.fontSize);
     const themeColor = useSelector(state => state.editorSettings.themeColor);
+    const showBlockControl = useSelector(state => state.editorSettings.showBlockControl);
+    const dispatch = useDispatch();
+
+    if (editableFields) {
+        return (
+
+            <VStack bg='' alignItems={'flex-start'} w='full'
+                padding={1}
+                borderRadius={'lg'}
+                position={'relative'}
+                outlineStyle={'solid'}
+                outlineColor={`${themeColor}.100`}
+                outlineWidth={(showBlockControl.show && showBlockControl.blockName == 'resumeSummary') ? '1px' : '0px'}
+                onMouseEnter={() => dispatch(setShowBlockControl({ blockName, show: true }))}
+                onMouseLeave={() => dispatch(setShowBlockControl({ blockName: null, show: false }))}
+            >
+                <SummaryFields dispatch={dispatch} editableFields={editableFields} />
+
+                {
+                    (showBlockControl.show && showBlockControl.blockName == 'resumeSummary') &&
+
+                    <BlockControlContainer blockName={'resumeSummary'} hideButtonAction={setResumeSummaryIsVisible} closeText={'Hide Summary block'}  >
+                        {/* add aditional controls here.. */}
+
+                        <Button
+                            aria-label="AI Assistant"
+                            variant={'outline'}
+                            bgColor={`white`}
+                            borderWidth={'1px'}
+                            borderColor={`${themeColor}.100`}
+                            _hover={{ backgroundColor: `${themeColor}.50` }}
+                            size={'xs'}
+                            rounded={'md'}
+                            px={1}
+                            onClick={() => dispatch(setShowModal({ blockName, show: true }))}
+                        ><LuSparkles />{'Ai Assistant' || `Click Me`}</Button>
+
+                    </BlockControlContainer>
+                }
+            </VStack >
+
+        )
+    }
+    else {
+        return (
+            // PDF render 
+            <div style={{
+                display: 'flex', flexDirection: 'column',
+                position: 'relative',
+                borderRadius: '0.5rem',
+                width: '100%',
+                padding: '0.25rem',
+                gap: '0.5rem',
+            }}
+            >
+                <SummaryFields dispatch={dispatch} editableFields={editableFields} />
+            </div>
+        )
+    }
+};
+
+export default SummaryBlock;
+
+const SummaryFields = ({ editableFields, dispatch }) => {
+    const fontSize = useSelector(state => state.fontSettings.fontSize);
     const summaryHeading = useSelector(state => state.resumeSummary.summaryHeading);
     const summaryText = useSelector(state => state.resumeSummary.summaryText);
 
-    const showBlockControl = useSelector(state => state.editorSettings.showBlockControl);
-
-    const dispatch = useDispatch();
 
     const onChangeHandler = (name, value,) => {
         dispatch(setResumeSummaryData({ name, value }));
     }
-
     return (
-
-        <VStack bg='' alignItems={'flex-start'} w='full'
-            padding={1}
-            borderRadius={'lg'}
-            position={'relative'}
-            outlineStyle={'solid'}
-            outlineColor={`${themeColor}.100`}
-            outlineWidth={(showBlockControl.show && showBlockControl.blockName == 'resumeSummary') ? '1px' : '0px'}
-            onMouseEnter={() => dispatch(setShowBlockControl({ blockName, show: true }))}
-            onMouseLeave={() => dispatch(setShowBlockControl({ blockName: null, show: false }))}
-        >
+        <>
             <CustomHeading
                 variant={'h2'}
                 size={fontSize.h2}
@@ -61,30 +112,6 @@ const SummaryBlock = ({ editableFields }) => {
                 isEditable={editableFields}
                 onChangeCallback={(name, value) => onChangeHandler(name, value)}
             />
-            {
-                (showBlockControl.show && showBlockControl.blockName == 'resumeSummary') &&
-
-
-                <BlockControlContainer blockName={'resumeSummary'} hideButtonAction={setResumeSummaryIsVisible} closeText={'Hide Summary block'}  >
-                    {/* add aditional controls here.. */}
-
-                    <Button
-                        aria-label="AI Assistant"
-                        variant={'outline'}
-                        bgColor={`white`}
-                        borderWidth={'1px'}
-                        borderColor={`${themeColor}.100`}
-                        _hover={{ backgroundColor: `${themeColor}.50` }}
-                        size={'xs'}
-                        rounded={'md'}
-                        px={1}
-                        onClick={() => dispatch(setShowModal({ blockName, show: true }))}
-                    ><LuSparkles />{'Ai Assistant' || `Click Me`}</Button>
-
-                </BlockControlContainer>
-            }
-        </VStack >
-    );
-};
-
-export default SummaryBlock;
+        </>
+    )
+}
