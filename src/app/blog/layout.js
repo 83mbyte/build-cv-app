0327c1416e44cf3c373ce491f985ed1c24da065f
83mbyte <registered.mail.mb@gmail.com>
2025-03-19 11:10:54 +0200

    blog layout styles updated

--- src/app/blog/layout.js
+++ src/app/blog/layout.js
@@ -1,5 +1,5 @@
-import { blogData } from "@/lib/content-lib"
-import Link from "next/link"
+import { blogData } from "@/lib/content-lib";
+import Link from "next/link";
 
 export const metadata = {
     title: `${process.env.NEXT_PUBLIC_APP_NAME} - Blog`,
@@ -10,22 +10,18 @@ export const metadata = {
 export default function BlogLayout({ children }) {
     return (
 
-        <section>
+        <div style={{ minHeight: '100vh', width: '100%', display: 'flex', flexDirection: 'column', justifyContent: 'space-between', alignItems: 'center', position: 'relative', }}>
 
-            <div style={{ minHeight: '100vh', width: '100%', display: 'flex', flexDirection: 'column', justifyContent: 'space-between', alignItems: 'center', position: 'relative' }}>
-                {/* TODO create styled header */}
-                {/* TODO create styled header */}
-                {/* TODO create styled header */}
-                <div style={{ backgroundColor: 'white', height: '30px', borderBottom: '1px solid gray', width: '100%', zIndex: 1000, position: 'fixed' }}><Link href='/'>{process.env.NEXT_PUBLIC_APP_NAME}</Link> the chill corner.</div>
+            <header style={{ backgroundColor: '', borderBottom: '0px solid gray', width: '100%', zIndex: 1000, padding: '0.75rem', position: 'fixed' }}>
+                <div style={{ color: 'white', fontSize: '1.125rem', backgroundColor: 'rgba(0, 0, 0, 0.64)', padding: '0.5rem', borderRadius: '0.375rem' }}><Link href='/'>{process.env.NEXT_PUBLIC_APP_NAME}</Link> the chill corner.</div>
+            </header>
 
-                <div style={{ marginTop: '40px', }}>
-                    {children}
-                </div>
+            <main style={{ marginTop: '40px', }}>
+                {children}
+            </main>
 
-                {/* TODO add footer */}
-                {/* TODO add footer */}
-                <div>footer copyright</div>
-            </div>
-        </section>
+            <footer style={{ fontSize: '0.5rem', marginBottom: '0.5rem' }}>{process.env.NEXT_PUBLIC_APP_NAME_FULL}&nbsp;©&nbsp;{new Date().getFullYear()}&nbsp;&nbsp;</footer>
+        </div>
+
     )
 }
