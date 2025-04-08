c36eb852c615af72cdfd990a633416a9806ca1d2
83mbyte <registered.mail.mb@gmail.com>
2025-04-08 13:55:29 +0300

    added link to ContactUs at FAQ section

--- src/components/index_page/faq/FAQ.js
+++ src/components/index_page/faq/FAQ.js
@@ -1,11 +1,12 @@
 
 import { useState } from "react";
-import { Text, VStack, Heading, Box, Accordion, Icon } from "@chakra-ui/react";
+import { Text, VStack, Heading, Box, Accordion, Icon, Link as ChakraLink } from "@chakra-ui/react";
 import { motion } from "motion/react";
 
 import { indexData } from '@/lib/content-lib';
 import { LuChevronDown } from "react-icons/lu";
 import SectionContainer from '../SectionContainer';
+import NextLink from "next/link";
 
 
 const faqData = Object.keys(indexData.faq.data).map((key) => {
@@ -47,9 +48,12 @@ const FAQ = () => {
                             </Accordion.ItemTrigger>
                             <Accordion.ItemContent >
                                 <Accordion.ItemBody padding={'2'} >
-                                    <Text textAlign={'left'} fontSize={['sm', 'md']}>
-                                        {item.a}
-                                    </Text>
+
+                                    {
+                                        index != 5
+                                            ? <Text textAlign={'left'} fontSize={['sm', 'md']}>{item.a}</Text>
+                                            : <Text textAlign={'left'} fontSize={['sm', 'md']}>{item.a[0]} <ChakraLink asChild colorPalette={'teal'} _focus={{ outline: 'none' }} variant={'underline'}><NextLink href={'/contact'}>Contact us</NextLink></ChakraLink> {item.a[1]}</Text>
+                                    }
                                 </Accordion.ItemBody>
                             </Accordion.ItemContent>
                         </Accordion.Item>
@@ -57,7 +61,7 @@ const FAQ = () => {
                 </Accordion.Root>
             </VStack>
 
-        </SectionContainer>
+        </SectionContainer >
     );
 };
 
